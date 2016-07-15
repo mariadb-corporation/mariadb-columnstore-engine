3933982f3ce562e7391e983970aecaf5e2f5948e
Ben Thompson <dl3hp33ui@gmail.com>
2016-07-15 10:43:56 -0500

    make installed scripts executable

--- utils/loggingcpp/genMsgAndErrId.sh
+++ utils/loggingcpp/genMsgAndErrId.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 ./genMsgId.pl > messageids-temp.h
-diff -abBq messageids-temp.h messageids.h >/dev/null 2>&1; if [ $? -ne 0 ]; then mv -f messageids-temp.h messageids.h; else touch messageids.h; fi;
+diff -abBq messageids-temp.h messageids.h >/dev/null 2>&1; if [ $? -ne 0 ]; then mv -f messageids-temp.h messageids.h; else touch -a messageids.h; fi;
 rm -f messageids-temp.h
 ./genErrId.pl > errorids-temp.h
-diff -abBq errorids-temp.h errorids.h >/dev/null 2>&1; if [ $? -ne 0 ]; then mv -f errorids-temp.h errorids.h; else touch errorids.h; fi;
+diff -abBq errorids-temp.h errorids.h >/dev/null 2>&1; if [ $? -ne 0 ]; then mv -f errorids-temp.h errorids.h; else touch -a errorids.h; fi;
 rm -f errorids-temp.h
