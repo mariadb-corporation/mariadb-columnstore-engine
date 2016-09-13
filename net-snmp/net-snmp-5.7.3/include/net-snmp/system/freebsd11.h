b954001fba714c683a8b8d2b26049e0e7d74b913
Andrew Hutchings <andrew.hutchings@mariadb.com>
2016-09-13 17:42:57 +0100

    Remove net-snmp and use the OS version
    Also remove a few old binaries from build/

--- net-snmp/net-snmp-5.7.3/include/net-snmp/system/freebsd11.h
+++
@@ -1,3 +0,0 @@
-/* freebsd11 is a superset of freebsd10 */
-#include "freebsd10.h"
-#define freebsd10 freebsd10
--- net-snmp/net-snmp-5.7.3/include/net-snmp/system/freebsd9.h
+++
@@ -1,3 +0,0 @@
-/* freebsd9 is a superset of freebsd8 */
-#include "freebsd8.h"
-#define freebsd8 freebsd8
