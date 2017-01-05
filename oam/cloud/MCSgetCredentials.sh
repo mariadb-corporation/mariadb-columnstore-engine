cc371071752a6f2800b20a60fcade535c40e5d49
David Hill <david.hill@mariadb.com>
2017-01-05 16:38:01 -0600

    MCOL-462

--- oam/cloud/MCSgetCredentials.sh
+++ oam/cloud/MCSgetCredentials.sh
@@ -1,6 +1,8 @@
 #! /bin/sh
 # Get Amazon EC2 security-credentials, access and secret access keys
 #
+prefix=/usr/local
+
 #first check for local versions, then meta-data versions
 if [ -f $HOME/.aws/credentials ]; then
 	exit 0
