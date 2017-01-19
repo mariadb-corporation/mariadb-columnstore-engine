61648c50eae563d46fa436586e04322878206169
David Hall <david.hall@mariadb.com>
2017-01-19 17:21:50 -0600

    MCOL-1 use getSystemSuspended() rather than is ReadWrite() to determine if system writes are suspended. Add REMOVE documentation to help file.

--- oamapps/mcsadmin/mcsadmin.cpp
+++ oamapps/mcsadmin/mcsadmin.cpp
@@ -804,7 +804,7 @@ int processCommand(string* arguments)
 				
 				BRM::DBRM dbrm;
 				// Ready to start the redistribute. The system must SuspendDataBaseWrites for the duration.
-				if (dbrm.isReadWrite() == BRM::ERR_OK)
+				if (dbrm.getSystemSuspended() == BRM::ERR_OK)
 				{
 					cout << "The system must be in read only mode for redistribeData to work" << endl;
 					cout << "You must run suspendDatabaseWrites before running redistributeData" << endl;
@@ -813,7 +813,7 @@ int processCommand(string* arguments)
 				}
 #if 0
 				// This can be used when redistributeData doesn't return until complete.
-				if (dbrm.isReadWrite() == ERR_OK)
+				if (dbrm.getSystemSuspended() == ERR_OK)
 				{
 					// System not in suspenddatabasewrites
 					// If there are bulkloads, ddl or dml happening, refuse the request
