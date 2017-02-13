6abd4a50a5fc7dfe9ff0ffd2dc36a3ae3177c763
Jean Baptiste Favre <debian@jbfavre.org>
2017-02-13 15:43:01 +0100

    Update except scripts to respect new '-m' option from postConfigure

--- oam/install_scripts/user_installer.sh
+++ oam/install_scripts/user_installer.sh
@@ -35,6 +35,7 @@ if { $UNM != "" } {
 if { $MYSQLPW == "none" } {
 	set MYSQLPW " "
 } 
+set DEPLOY_PACKAGES [lindex $argv 12]
 
 set BASH "/bin/bash "
 #if { $DEBUG == "1" } {
@@ -46,24 +47,25 @@ set HOME "$env(HOME)"
 log_user $DEBUG
 spawn -noecho /bin/bash
 #
-if { $PKGTYPE == "rpm" } {
-	set PKGERASE "rpm -e --nodeps \$(rpm -qa | grep '^mariadb-columnstore')"
-	set PKGERASE1 "rpm -e --nodeps "
+if { $DEPLOY_PACKAGES } {
+    if { $PKGTYPE == "rpm" } {
+        set PKGERASE "rpm -e --nodeps \$(rpm -qa | grep '^mariadb-columnstore')"
+        set PKGERASE1 "rpm -e --nodeps "
 
-	set PKGINSTALL "rpm -ivh $NODEPS --force mariadb-columnstore*$VERSION*rpm"
-	set PKGUPGRADE "rpm -Uvh --noscripts mariadb-columnstore*$VERSION*rpm"
-} else {
-	if { $PKGTYPE == "deb" } {
-		set PKGERASE "dpkg -P \$(dpkg --get-selections | grep '^mariadb-columnstore')"
-		set PKGERASE1 "dpkg -P "
-		set PKGINSTALL "dpkg -i --force-confnew mariadb-columnstore*$VERSION*deb"
-		set PKGUPGRADE "dpkg -i --force-confnew mariadb-columnstore*$VERSION*deb"
-	} else {
-		send_user "Invalid Package Type of $PKGTYPE"
-		exit 1
-	}
+        set PKGINSTALL "rpm -ivh $NODEPS --force mariadb-columnstore*$VERSION*rpm"
+        set PKGUPGRADE "rpm -Uvh --noscripts mariadb-columnstore*$VERSION*rpm"
+    } else {
+        if { $PKGTYPE == "deb" } {
+            set PKGERASE "dpkg -P \$(dpkg --get-selections | grep '^mariadb-columnstore')"
+            set PKGERASE1 "dpkg -P "
+            set PKGINSTALL "dpkg -i --force-confnew mariadb-columnstore*$VERSION*deb"
+            set PKGUPGRADE "dpkg -i --force-confnew mariadb-columnstore*$VERSION*deb"
+        } else {
+            send_user "Invalid Package Type of $PKGTYPE"
+            exit 1
+        }
+    }
 }
-
 # check and see if remote server has ssh keys setup, set PASSWORD if so
 send_user " "
 send "ssh $USERNAME@$SERVER 'time'\n"
@@ -71,7 +73,7 @@ set timeout 60
 expect {
 	"Host key verification failed" { send_user "FAILED: Host key verification failed\n" ; exit 1 }
 	"service not known" { send_user "FAILED: Invalid Host\n" ; exit 1 }
-	"authenticity" { send "yes\n" 
+	"authenticity" { send "yes\n"
 						expect {
 							"word: " { send "$PASSWORD\n" }
 							"passphrase" { send "$PASSWORD\n" }
@@ -95,109 +97,87 @@ send_user "\n"
 #BUG 5749 - SAS: didn't work on their system until I added the sleep 60
 sleep 60
 
-if { $INSTALLTYPE == "initial" || $INSTALLTYPE == "uninstall" } {
-	# 
-	# erase MariaDB Columnstore packages
-	#
-	send_user "Erase MariaDB Columnstore Packages on Module           "
-	send "ssh $USERNAME@$SERVER '$PKGERASE ;$PKGERASE1 dummy'\n"
-	if { $PASSWORD != "ssh" } {
-		set timeout 30
-		expect {
-			"word: " { send "$PASSWORD\n" }
-			"passphrase" { send "$PASSWORD\n" }
-		}
-	}
-	set timeout 120
-	expect {
-		"package dummy" { send_user "DONE" }
-		"error: Failed dependencies" { send_user "ERROR: Failed dependencies\n" ; exit 1 }
-		"Permission denied, please try again"   { send_user "ERROR: Invalid password\n" ; exit 1 }
-		"Connection closed"   { send_user "ERROR: Connection closed\n" ; exit 1 }
-	}
-	send_user "\n"
+if { $DEPLOY_PACKAGES } {
+    if { $INSTALLTYPE == "initial" || $INSTALLTYPE == "uninstall" } {
+        #
+        # erase MariaDB Columnstore packages
+        #
+        send_user "Erase MariaDB Columnstore Packages on Module           "
+        send "ssh $USERNAME@$SERVER '$PKGERASE ;$PKGERASE1 dummy'\n"
+        if { $PASSWORD != "ssh" } {
+            set timeout 30
+            expect {
+                "word: " { send "$PASSWORD\n" }
+                "passphrase" { send "$PASSWORD\n" }
+            }
+        }
+        set timeout 120
+        expect {
+            "package dummy" { send_user "DONE" }
+            "error: Failed dependencies" { send_user "ERROR: Failed dependencies\n" ; exit 1 }
+            "Permission denied, please try again"   { send_user "ERROR: Invalid password\n" ; exit 1 }
+            "Connection closed"   { send_user "ERROR: Connection closed\n" ; exit 1 }
+        }
+        send_user "\n"
+    }
 }
 
 if { $INSTALLTYPE == "uninstall" } { exit 0 }
 
-# 
-# send the MariaDB ColumnStore package
-#
-set timeout 30
-#expect -re {[$#] }
-send_user "Copy new MariaDB Columnstore Packages to Module              "
-send "ssh $USERNAME@$SERVER 'rm -f /root/mariadb-columnstore-*.$PKGTYPE'\n"
-if { $PASSWORD != "ssh" } {
-	set timeout 30
-	expect {
-		"word: " { send "$PASSWORD\n" }
-		"passphrase" { send "$PASSWORD\n" }
-		"Connection closed"   { send_user "ERROR: Connection closed\n" ; exit 1 }
-	}
-}
-expect {
-        -re {[$#] } { }
-        "Connection refused"   { send_user "ERROR: Connection refused\n" ; exit 1 }
+if { $DEPLOY_PACKAGES } {
+
+    send "scp $HOME/mariadb-columnstore*$VERSION*$PKGTYPE $USERNAME@$SERVER:.;$PKGERASE1 dummy\n"
+    if { $PASSWORD != "ssh" } {
+        set timeout 30
+        expect {
+            "word: " { send "$PASSWORD\n" }
+            "passphrase" { send "$PASSWORD\n" }
+        }
+    }
+    set timeout 120
+    expect {
+        "package dummy" 	{ send_user "DONE" }
+        "directory"  		{ send_user "ERROR\n" ;
+                        send_user "\n*** Installation ERROR\n" ;
+                        exit 1 }
         "Connection closed"   { send_user "ERROR: Connection closed\n" ; exit 1 }
-        "No route to host"   { send_user "ERROR: No route to host\n" ; exit 1 }
-}
-set timeout 30
-expect {
-	-re {[$#] } { }
-}
+    }
+    send_user "\n"
 
-send "scp $HOME/mariadb-columnstore*$VERSION*$PKGTYPE $USERNAME@$SERVER:.;$PKGERASE1 dummy\n"
-if { $PASSWORD != "ssh" } {
-	set timeout 30
-	expect {
-		"word: " { send "$PASSWORD\n" }
-		"passphrase" { send "$PASSWORD\n" }
-	}
+    #sleep to make sure it's finished
+    sleep 5
+    #
+    set timeout 30
+    expect -re {[$#] }
+    if { $INSTALLTYPE == "initial"} {
+        #
+        # install package
+        #
+        send_user "Install MariaDB ColumnStore Packages on Module               "
+        send "ssh $USERNAME@$SERVER '$PKGINSTALL ;$PKGERASE1 dummy'\n"
+        if { $PASSWORD != "ssh" } {
+            set timeout 30
+            expect {
+                "word: " { send "$PASSWORD\n" }
+                "passphrase" { send "$PASSWORD\n" }
+            }
+        }
+        set timeout 180
+        expect {
+            "package dummy" 	  { send_user "DONE" }
+            "error: Failed dependencies" { send_user "ERROR: Failed dependencies\n" ;
+                                send_user "\n*** Installation ERROR\n" ;
+                                exit 1 }
+            "Connection closed"   { send_user "ERROR: Connection closed\n" ; exit 1 }
+            "needs"	   { send_user "ERROR: disk space issue\n" ; exit 1 }
+            "conflicts"	   { send_user "ERROR: File Conflict issue\n" ; exit 1 }
+        }
+        send_user "\n"
+        set timeout 30
+    }
+    #sleep to make sure it's finished
+    sleep 5
 }
-set timeout 120
-expect {
-	"package dummy" 	{ send_user "DONE" }
-	"directory"  		{ send_user "ERROR\n" ; 
-				 	send_user "\n*** Installation ERROR\n" ; 
-					exit 1 }
-	"Connection closed"   { send_user "ERROR: Connection closed\n" ; exit 1 }
-}
-send_user "\n"
-
-#sleep to make sure it's finished
-sleep 5
-#
-set timeout 30
-expect -re {[$#] }
-if { $INSTALLTYPE == "initial"} {
-	#
-	# install package
-	#
-	send_user "Install MariaDB ColumnStore Packages on Module               "
-	send "ssh $USERNAME@$SERVER '$PKGINSTALL ;$PKGERASE1 dummy'\n"
-	if { $PASSWORD != "ssh" } {
-		set timeout 30
-		expect {
-			"word: " { send "$PASSWORD\n" }
-			"passphrase" { send "$PASSWORD\n" }
-		}
-	}
-	set timeout 180
-	expect {
-		"package dummy" 	  { send_user "DONE" }
-		"error: Failed dependencies" { send_user "ERROR: Failed dependencies\n" ; 
-							send_user "\n*** Installation ERROR\n" ; 
-							exit 1 }
-		"Connection closed"   { send_user "ERROR: Connection closed\n" ; exit 1 }
-		"needs"	   { send_user "ERROR: disk space issue\n" ; exit 1 }
-		"conflicts"	   { send_user "ERROR: File Conflict issue\n" ; exit 1 }
-	}
-	send_user "\n"
-
-	set timeout 30
-}
-#sleep to make sure it's finished
-sleep 5
 #
 if { $INSTALLTYPE == "initial"} {
 	#
