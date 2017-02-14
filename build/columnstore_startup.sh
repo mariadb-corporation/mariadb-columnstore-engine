7e8c97f65dfa5dc8890f6356c6d293bd1dac0295
David Hill <david.hill@mariadb.com>
2017-02-14 15:21:31 -0600

    MCOL-509 - add columnstore startup script, used in buildbot

---
+++ build/columnstore_startup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# $Id: columnstore_startup.sh 3705 2013-08-07 19:47:20Z dhill $
+#
+# columnstore_startup.sh steps for columnstore single server startup after install
+
+prefix=/usr/local
+installdir=$prefix/mariadb/columnstore
+
+quiet=0
+
+for arg in "$@"; do
+	if [ `expr -- "$arg" : '--prefix='` -eq 9 ]; then
+		prefix="`echo $arg | awk -F= '{print $2}'`"
+		installdir=$prefix/mariadb/columnstore
+	elif [ `expr -- "$arg" : '--installdir='` -eq 13 ]; then
+		installdir="`echo $arg | awk -F= '{print $2}'`"
+		prefix=`dirname $installdir`
+	else
+		echo "columnstore_startup.sh: ignoring unknown argument: $arg" 1>&2
+	fi
+done
+
+$installdir/bin/post-install --installdir=/usr/local/mariadb/columnstore
+echo -e "1\ncs-1\n1\n1\n" | $installdir/bin/postConfigure -i $installdir
+exit 0
