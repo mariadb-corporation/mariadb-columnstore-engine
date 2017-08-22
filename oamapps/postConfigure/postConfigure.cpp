7aa4c98c89e3f66523dcdb68ed65ac0de04d65d4
david hill <david.hill@mariadb.com>
2017-08-22 13:08:13 -0500

    change to look for packages versus gz file

--- oamapps/postConfigure/postConfigure.cpp
+++ oamapps/postConfigure/postConfigure.cpp
@@ -2841,13 +2841,12 @@ int main(int argc, char *argv[])
 		    
 			//check if pkgs are located in $HOME directory
 			if ( EEPackageType == "rpm")
-				columnstorePackage = HOME + "/" + "mariadb-columnstore-" + version + "*.rpm.tar.gz";
+				columnstorePackage = HOME + "/" + "mariadb-columnstore-" + version + "*.rpm";
 			else
 				if ( EEPackageType == "deb") 
-					columnstorePackage = HOME + "/" + "mariadb-columnstore-" + version + "*.deb.tar.gz";
+					columnstorePackage = HOME + "/" + "mariadb-columnstore-" + version + "*.deb";
 				else
 					columnstorePackage = HOME + "/" + "mariadb-columnstore-" + version + "*.bin.tar.gz";
-
 
 			if( !pkgCheck(columnstorePackage) )
 			exit(1);
