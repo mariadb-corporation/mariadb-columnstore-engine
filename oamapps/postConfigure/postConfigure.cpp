44736966e17918e6b34494ae9b9e1707cf0371e3
david hill <david.hill@mariadb.com>
2017-10-24 10:11:55 -0500

    mcol-667 - change from using bashrc to profile files

--- oamapps/postConfigure/postConfigure.cpp
+++ oamapps/postConfigure/postConfigure.cpp
@@ -3518,6 +3518,8 @@ int main(int argc, char *argv[])
 
 		cout << "Enter 'mcsmysql' to access the MariaDB ColumnStore SQL console" << endl;
 		cout << "Enter 'mcsadmin' to access the MariaDB ColumnStore Admin console" << endl << endl;
+		
+		cout << "NOTE: The MariaDB ColumnStore Alias Commands are also in /etc/profile.d/columnstoreAlias" << endl << endl;
 	}
 	else
 	{
