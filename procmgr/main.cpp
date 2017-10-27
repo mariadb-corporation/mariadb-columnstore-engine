8a1d1f7aa8ee9bb533b522a684a2a79b93666780
david hill <david.hill@mariadb.com>
2017-10-27 13:41:22 -0500

    add DistributedInstall to singlserver xml file, fix comment in procmgr

--- procmgr/main.cpp
+++ procmgr/main.cpp
@@ -563,7 +563,7 @@ static void startMgrProcessThread()
         }
         catch (...) 
 	{
-                log.writeLog(__LINE__, "addModule - ERROR: get DistributedInstall", LOG_TYPE_ERROR);
+                log.writeLog(__LINE__, "ERROR: get DistributedInstall", LOG_TYPE_ERROR);
 	}
 
 	//Send out a start service just to make sure Columnstore is runing on remote nodes
