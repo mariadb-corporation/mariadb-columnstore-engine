9c6e1dabf7b22db69582fdba3e55f8f715da46f8
Ben Thompson <ben.thompson@mariadb.com>
2018-04-16 13:48:08 -0500

    debug pipestatus output

--- build/columnstore_startup.sh
+++ build/columnstore_startup.sh
@@ -23,6 +23,7 @@ done
 
 $installdir/bin/post-install --installdir=/usr/local/mariadb/columnstore
 echo -e "1\n1\n1\n1\n1\n1\n" | $installdir/bin/postConfigure -i $installdir
+echo ${PIPESTATUS[@]}
 if [ ${PIPESTATUS[1]} -eq 0 ]; then
 	exit 0
 else
