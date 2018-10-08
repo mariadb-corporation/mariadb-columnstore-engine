f48b92e46f00f09abb46f606e2e49d117ca994ca
David Hill <david.hill@mariadb.com>
2018-10-08 13:46:49 -0500

    MCOL-520

--- procmon/main.cpp
+++ procmon/main.cpp
@@ -180,7 +180,7 @@ int main(int argc, char** argv)
     //get tmp log directory
     tmpLogDir = startup::StartUp::tmpDir();
     
-    string cmd = "mkdir -p tmpLogDir";
+    string cmd = "mkdir -p " + tmpLogDir;
     system(cmd.c_str());
 
     // create message thread
