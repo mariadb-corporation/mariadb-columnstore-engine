20b0d3b88b23dc018ccd4f4b64ddddc9df447798
Patrick LeBlanc <patrick.leblanc@mariadb.com>
2019-01-17 10:34:19 -0600

    initial commit of the message format definitions.

---
+++ include/messageFormat.h
@@ -0,0 +1,23 @@
+// Eventually this will define stuff client code will use to
+// construct messages to StorageManager.
+
+#ifndef MESSAGEFORMAT_H_
+#define MESSAGEFORMAT_H_
+
+namespace storagemanager
+{
+
+#define SM_MSG_START 0xbf65a7e1
+
+enum Opcodes {
+    OPEN,
+    READ,
+    WRITE,
+    STAT,
+    UNLINK
+};
+
+}
+
+#endif
+
