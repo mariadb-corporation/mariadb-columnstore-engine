7e86993d58269635dff8f23ab479adb89a6b20ce
Patrick LeBlanc <patrick.leblanc@mariadb.com>
2019-02-04 17:26:24 -0600

    Fixed a typo/bug in the listdirectory response length.

--- src/ListDirectoryTask.cpp
+++ src/ListDirectoryTask.cpp
@@ -81,7 +81,7 @@ bool ListDirectoryTask::run()
     // be careful modifying the listdir return types...
     uint payloadLen = sizeof(listdir_resp_entry) * listing.size();
     for (uint i = 0; i < listing.size(); i++)
-        payloadLen += listing.size();
+        payloadLen += listing[i].size();
     
     sm_msg_resp *resp = (sm_msg_resp *) buf;
     resp->type = SM_MSG_START;
