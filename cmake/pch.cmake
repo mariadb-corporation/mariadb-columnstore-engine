aa4b305e4c60c11570ad5306ac1c44b91e24551a
David Mott <david.mott@mariadb.com>
2019-06-11 15:08:13 -0500

    disable PCH by default

--- cmake/pch.cmake
+++ cmake/pch.cmake
@@ -25,12 +25,37 @@ execute_process(COMMAND ${CMAKE_COMMAND} --build . WORKING_DIRECTORY ${COTIRE_DO
 LIST(APPEND CMAKE_MODULE_PATH ${COTIRE_DIR}/CMake)
 include(cotire)
 
+
+set(_PCH_HEADER)
+configure_file(${CMAKE_CURRENT_LIST_DIR}/pch.cpp.in ${CMAKE_CURRENT_BINARY_DIR}/pch.cpp @ONLY)
+add_library(pch STATIC 
+  ${CMAKE_CURRENT_LIST_DIR}/pch.cpp.in 
+  ${CMAKE_CURRENT_BINARY_DIR}/pch.cpp 
+  ${CMAKE_CURRENT_BINARY_DIR}/columnstore.h)
+set_target_properties(pch PROPERTIES COTIRE_CXX_PREFIX_HEADER_INIT "${CMAKE_CURRENT_BINARY_DIR}/columnstore.h")
+set(COTIRE_MINIMUM_NUMBER_OF_TARGET_SOURCES 1)
+cotire(pch)
+get_target_property(_PCH_HEADER pch COTIRE_CXX_PREFIX_HEADER)
+
+message("_PCH_HEADER: ${_PCH_HEADER}")
+
+add_custom_command(TARGET pch POST_BUILD
+  COMMAND ${CMAKE_COMMAND} -E create_symlink ${_PCH_HEADER}.gch "${CMAKE_CURRENT_BINARY_DIR}/columnstore.h.gch"
+)
+
 #[[
   target_pch( <target name> [ <pch header> ] )
 
   if the PCH header parameter is missing they will be automatically deduced
 ]]
 
+function(target_pch _target)
+#  add_dependencies(${_target} pch)
+#  set_target_properties(${_target} PROPERTIES COTIRE_CXX_PREFIX_HEADER_INIT "${_PCH_HEADER}")
+#  cotire(${_target})
+endfunction()
+
+#[[
 function(target_pch _target)
   get_target_property(_sources ${_target} SOURCES)
   get_target_property(_srcdir ${_target} SOURCE_DIR)
@@ -50,6 +75,7 @@ function(target_pch _target)
   else()
     set_target_properties(${_target} PROPERTIES COTIRE_CXX_PREFIX_HEADER_INIT "${columnstore_BINARY_DIR}/columnstore.h")
   endif()
-
+  set_target_properties(${_target} PROPERTIES COTIRE_CXX_PREFIX_HEADER_INIT "${_PCH_HEADER}")
   cotire(${_target})
 endfunction()
+  ]]
