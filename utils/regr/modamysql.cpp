6ea3671efa73ce0585c16035437fb7ddbfbbe1c2
David Hall <mcfadden7586@gmail.com>
2019-09-24 16:42:15 -0500

    MCOL-3503 Finish the Server portion

--- utils/regr/modamysql.cpp
+++ utils/regr/modamysql.cpp
@@ -4,6 +4,7 @@
 #include <sstream>
 #include <string.h>
 #include <tr1/unordered_map>
+#include <algorithm>
 
 #include "idb_mysql.h"
 
@@ -26,210 +27,23 @@ inline bool isNumeric(int type, const char* attr)
     return false;
 }
 
-template<class T>
-inline T cvtArg(int t, char* v, uint32_t len)
-{
-    T d = 0.0;
-    
-    if (t == DECIMAL_RESULT || t == STRING_RESULT)
-    {
-        // NULL terminate the string
-        // This can be a problem if Server has adjoining non zero terminated buffers
-        *(v+len) = 0;
-        d = strtold(v, 0);
-    }
-    else
-        d = *((T*)v);
-
-    return d;
-}
-
-/****************************************************************************
- * UDF function moda
- * MariaDB's UDF function creation guideline needs to be followed.
- * 
- * moda returns the value with the greatest number of occurances in
- * the dataset with ties being broken by:
- * 1) closest to AVG
- * 2) smallest value
- */
-
 struct moda_data
 {
     long double fSum;
     uint64_t    fCount;
-    void*       fMap; // Will be of type unordered_map<>
     enum Item_result    fReturnType; 
-//    Moda_impl_base* modaImpl; // A pointer to one of the Moda_impl_T concrete classes
-    
-    template<class T>
-    std::tr1::unordered_map<T, uint32_t>* getMap() 
-    {
-        if (!fMap)
-        {
-            // Just in time creation
-            fMap = new std::tr1::unordered_map<T, uint32_t>;
-        }
-        return (std::tr1::unordered_map<T, uint32_t>*)fMap;
-    }
-    
-    template<class T>
+    std::tr1::unordered_map<int64_t, uint32_t> mapINT;
+    std::tr1::unordered_map<double, uint32_t> mapREAL;
+    std::tr1::unordered_map<long double, uint32_t> mapDECIMAL;
     void clear()
     {
         fSum = 0.0;
         fCount = 0;
-        getMap<T>()->clear();
+        mapINT.clear();
+        mapREAL.clear();
+        mapDECIMAL.clear();
     }
 };
-
-
-class Moda_impl_base
-{
-public:
-    // Defaults OK
-    Moda_impl_base() {};
-    virtual ~Moda_impl_base() {};
-
-    virtual my_bool moda_init(UDF_INIT* initid, 
-                              UDF_ARGS* args, 
-                              char* message);
-    virtual void moda_deinit(UDF_INIT* initid);
-    virtual void moda_clear(UDF_INIT* initid);
-    virtual void moda_add(UDF_INIT* initid, 
-                          UDF_ARGS* args,
-                          char* is_null,
-                          char* message __attribute__((unused)));
-    virtual void moda_remove(UDF_INIT* initid, 
-                             UDF_ARGS* args,
-                             char* is_null,
-                             char* message __attribute__((unused)));
-    virtual char* moda(UDF_INIT* initid, 
-                       UDF_ARGS* args __attribute__((unused)),
-                       char* is_null, 
-                       char* error __attribute__((unused)));
-};
-
-template<class T>
-class Moda_impl : public Moda_impl_base
-{
-public:
-    // Defaults OK
-    Moda_impl() {};
-    virtual ~Moda_impl() {};
-
-    my_bool moda_init(UDF_INIT* initid, 
-                      UDF_ARGS* args, 
-                      char* message)
-    {
-        return (my_bool)TRUE;
-    }
-
-    void moda_deinit(UDF_INIT* initid)
-    {
-        struct moda_data* data = (struct moda_data*)initid->ptr;
-        std::tr1::unordered_map<T, uint32_t>* map = data->getMap<T>();
-        data->clear<T>();
-        delete map;
-        delete data;
-    }   
-    
-    void moda_clear(UDF_INIT* initid)
-    {
-        struct moda_data* data = (struct moda_data*)initid->ptr;
-        data->clear<T>();
-    }
-
-    void moda_add(UDF_INIT* initid, 
-                  UDF_ARGS* args,
-                  char* is_null,
-                  char* message __attribute__((unused)))
-    {
-        struct moda_data* data = (struct moda_data*)initid->ptr;
-        std::tr1::unordered_map<T, uint32_t>* map = data->getMap<T>();
-        T val = cvtArg<T>(args->arg_type[0], args->args[0], args->lengths[0]);
-        data->fSum += val;
-        ++data->fCount;
-        (*map)[val]++;
-    }
-
-    void moda_remove(UDF_INIT* initid, 
-                     UDF_ARGS* args,
-                     char* is_null,
-                     char* message __attribute__((unused)))
-    {
-        struct moda_data* data = (struct moda_data*)initid->ptr;
-        std::tr1::unordered_map<T, uint32_t>* map = data->getMap<T>();
-        long double val = cvtArg<T>(args->arg_type[0], args->args[0], args->lengths[0]);
-        data->fSum -= val;
-        --data->fCount;
-        (*map)[val]--;
-    }
-
-    char* moda(UDF_INIT* initid, 
-               UDF_ARGS* args __attribute__((unused)),
-               char* is_null, char* 
-               error __attribute__((unused)))
-    {
-        struct moda_data* data = (struct moda_data*)initid->ptr;
-        std::tr1::unordered_map<T, uint32_t>* map = data->getMap<T>();
-        long double avg = data->fCount ? data->fSum / data->fCount : 0;
-        long double maxCnt = 0.0;
-        T val = 0.0;
-
-        typename std::tr1::unordered_map<T, uint32_t>::iterator iter;
-
-        for (iter = map->begin(); iter != map->end(); ++iter)
-        {
-            if (iter->second > maxCnt)
-            {
-                val = iter->first;
-                maxCnt = iter->second;
-            }
-            else if (iter->second == maxCnt)
-            {
-                // Tie breaker: choose the closest to avg. If still tie, choose smallest
-                if ((abs(val-avg) > abs(iter->first-avg))
-                || ((abs(val-avg) == abs(iter->first-avg)) && (abs(val) > abs(iter->first))))
-                {
-                    val = iter->first;
-                }
-            }
-        }
-        std::ostringstream oss;
-        oss << val;
-        return const_cast<char*>(oss.str().c_str());
-    }
-};
-
-Moda_impl<int64_t>     moda_impl_int64;
-Moda_impl<double>      moda_impl_double;
-Moda_impl<long double> moda_impl_longdouble;
-
-Moda_impl_base* getImpl(UDF_INIT* initid)
-{
-    struct moda_data* data = (struct moda_data*)initid->ptr;
-    if (!data)
-        return NULL;
-    if (data->modaImpl)
-        return data->modaImpl;
-    
-    switch (data->fReturnType)
-    {
-        case INT_RESULT:
-            data->modaImpl = &moda_impl_int64;
-            break;
-        case REAL_RESULT:
-            data->modaImpl = &moda_impl_double;
-            break;
-        case DECIMAL_RESULT:
-        case STRING_RESULT:
-            data->modaImpl = &moda_impl_int64;
-            break;
-        default:
-            data->modaImpl = NULL;
-    }
-    return data->modaImpl;
-}
 
 }
 
@@ -258,7 +72,6 @@ my_bool moda_init(UDF_INIT* initid, UDF_ARGS* args, char* message)
     data->fCount = 0;
     data->fSum = 0.0;
     initid->ptr = (char*)data;
-    getImpl(initid);
     return 0;
 }
 
@@ -267,9 +80,9 @@ __declspec(dllexport)
 #endif
 void moda_deinit(UDF_INIT* initid)
 {
-    Moda_impl_base* impl = getImpl(initid);
-    if (impl)
-        impl->moda_deinit(initid);
+    struct moda_data* data = (struct moda_data*)initid->ptr;
+    data->clear();
+    delete data;
 }   
 
 #ifdef _MSC_VER
@@ -278,9 +91,8 @@ __declspec(dllexport)
 void moda_clear(UDF_INIT* initid, char* is_null __attribute__((unused)),
                 char* message __attribute__((unused)))
 {
-    Moda_impl_base* impl = getImpl(initid);
-    if (impl)
-        impl->moda_clear(initid);
+    struct moda_data* data = (struct moda_data*)initid->ptr;
+    data->clear();
 }
 
 #ifdef _MSC_VER
@@ -296,9 +108,37 @@ void moda_add(UDF_INIT* initid,
     {
         return;
     }
-    Moda_impl_base* impl = getImpl(initid);
-    if (impl)
-        impl->moda_add(initid, args, is_null, message);
+
+    struct moda_data* data = (struct moda_data*)initid->ptr;
+    data->fCount++;
+
+    switch (args->arg_type[0])
+    {
+        case INT_RESULT:
+        {
+            int64_t val = *((int64_t*)args->args[0]);
+            data->fSum += (long double)val;
+            data->mapINT[val]++;
+            break;
+        }
+        case REAL_RESULT:
+        {
+            double val = *((double*)args->args[0]);
+            data->fSum += val;
+            data->mapREAL[val]++;
+            break;
+        }
+        case DECIMAL_RESULT:
+        case STRING_RESULT:
+        {
+            long double val = strtold(args->args[0], 0);
+            data->fSum += val;
+            data->mapDECIMAL[val]++;
+            break;
+        }
+        default:
+            break;
+    }
 }
 
 #ifdef _MSC_VER
@@ -313,9 +153,36 @@ void moda_remove(UDF_INIT* initid, UDF_ARGS* args,
     {
         return;
     }
-    Moda_impl_base* impl = getImpl(initid);
-    if (impl)
-        impl->moda_remove(initid, args, is_null, message);
+    struct moda_data* data = (struct moda_data*)initid->ptr;
+    data->fCount--;
+
+    switch (args->arg_type[0])
+    {
+        case INT_RESULT:
+        {
+            int64_t val = *((int64_t*)args->args[0]);
+            data->fSum -= (long double)val;
+            data->mapINT[val]--;
+            break;
+        }
+        case REAL_RESULT:
+        {
+            double val = *((double*)args->args[0]);
+            data->fSum -= val;
+            data->mapREAL[val]--;
+            break;
+        }
+        case DECIMAL_RESULT:
+        case STRING_RESULT:
+        {
+            long double val = strtold(args->args[0], 0);
+            data->fSum -= val;
+            data->mapDECIMAL[val]--;
+            break;
+        }
+        default:
+            break;
+    }
 }
 
 #ifdef _MSC_VER
@@ -324,9 +191,97 @@ __declspec(dllexport)
 char* moda(UDF_INIT* initid, UDF_ARGS* args __attribute__((unused)),
            char* is_null, char* error __attribute__((unused)))
 {
-    Moda_impl_base* impl = getImpl(initid);
-    if (impl)
-        return impl->moda(initid, args, is_null, error);
+    struct moda_data* data = (struct moda_data*)initid->ptr;
+    uint32_t maxCnt = 0.0;
+
+    switch (args->arg_type[0])
+    {
+        case INT_RESULT:
+        {
+            typename std::tr1::unordered_map<int64_t, uint32_t>::iterator iter;
+            int64_t avg = (int64_t)data->fCount ? data->fSum / data->fCount : 0;
+            int64_t val = 0.0;
+            for (iter = data->mapINT.begin(); iter != data->mapINT.end(); ++iter)
+            {
+                if (iter->second > maxCnt)
+                {
+                    val = iter->first;
+                    maxCnt = iter->second;
+                }
+                else if (iter->second == maxCnt)
+                {
+                    // Tie breaker: choose the closest to avg. If still tie, choose smallest
+                    if ((abs(val-avg) > abs(iter->first-avg))
+                    || ((abs(val-avg) == abs(iter->first-avg)) && (abs(val) > abs(iter->first))))
+                    {
+                        val = iter->first;
+                    }
+                }
+            }
+            std::ostringstream oss;
+            oss << val;
+            return const_cast<char*>(oss.str().c_str());
+            break;
+        }
+        case REAL_RESULT:
+        {
+            typename std::tr1::unordered_map<double, uint32_t>::iterator iter;
+            double avg = data->fCount ? data->fSum / data->fCount : 0;
+            double val = 0.0;
+            for (iter = data->mapREAL.begin(); iter != data->mapREAL.end(); ++iter)
+            {
+                if (iter->second > maxCnt)
+                {
+                    val = iter->first;
+                    maxCnt = iter->second;
+                }
+                else if (iter->second == maxCnt)
+                {
+                    // Tie breaker: choose the closest to avg. If still tie, choose smallest
+                    if ((abs(val-avg) > abs(iter->first-avg))
+                    || ((abs(val-avg) == abs(iter->first-avg)) && (abs(val) > abs(iter->first))))
+                    {
+                        val = iter->first;
+                    }
+                }
+            }
+            std::ostringstream oss;
+            oss << val;
+            return const_cast<char*>(oss.str().c_str());
+            break;
+        }
+        case DECIMAL_RESULT:
+        case STRING_RESULT:
+        {
+            typename std::tr1::unordered_map<long double, uint32_t>::iterator iter;
+            long double avg = data->fCount ? data->fSum / data->fCount : 0;
+            long double val = 0.0;
+            for (iter = data->mapDECIMAL.begin(); iter != data->mapDECIMAL.end(); ++iter)
+            {
+                if (iter->second > maxCnt)
+                {
+                    val = iter->first;
+                    maxCnt = iter->second;
+                }
+                else if (iter->second == maxCnt)
+                {
+                    long double thisVal = iter->first;
+                    // Tie breaker: choose the closest to avg. If still tie, choose smallest
+                    if ((abs(val-avg) > abs(thisVal-avg))
+                    || ((abs(val-avg) == abs(thisVal-avg)) && (abs(val) > abs(thisVal))))
+                    {
+                        val = thisVal;
+                    }
+                }
+            }
+            std::ostringstream oss;
+            oss << val;
+            return const_cast<char*>(oss.str().c_str());
+            break;
+        }
+        default:
+            break;
+    }
     return NULL;
 }
 
