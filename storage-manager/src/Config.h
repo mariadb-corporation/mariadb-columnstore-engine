4ad82423697ac7e32e35a893b804ffe2f38ed0b3
benthompson15 <ben.thompson.015@gmail.com>
2019-10-07 13:16:52 -0500

    MCOL-3357: leftover unsed code removed.

--- storage-manager/src/Config.h
+++ storage-manager/src/Config.h
@@ -39,8 +39,6 @@ class Config : public boost::noncopyable
         
         // for testing, lets caller specify a config file to use
         static Config *get(const std::string &);
-        // for inotify to get filename
-        std::string getFilename(){return filename;}
 
         void checkConfigChanges();
 
