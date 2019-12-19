dbc8cbaca99e5bdc307accb9ce696b2e4bd54bc8
Patrick LeBlanc <patrick.leblanc@mariadb.com>
2019-12-19 18:08:49 -0500

    Revert "Merge pull request #989 from LinuxJedi/set-maturity"
    This reverts commit 5eb050f3af30cb51d1a71f7d8565708f8fa43656, reversing
    changes made to 0be00a3a737b226e656b390326606bb30cf77816.

--- dbcon/mysql/ha_mcs.cpp
+++ dbcon/mysql/ha_mcs.cpp
@@ -26,10 +26,6 @@
 #include "is_columnstore.h"
 #include "ha_mcs_version.h"
 
-#ifndef COLUMNSTORE_MATURITY
-#define COLUMNSTORE_MATURITY MariaDB_PLUGIN_MATURITY_STABLE
-#endif
-
 static handler* calpont_create_handler(handlerton* hton,
                                        TABLE_SHARE* table,
                                        MEM_ROOT* mem_root);
@@ -932,19 +928,19 @@ static struct st_mysql_information_schema is_columnstore_plugin_version =
 
 maria_declare_plugin(columnstore)
 {
-    MYSQL_STORAGE_ENGINE_PLUGIN,
-    &columnstore_storage_engine,
-    "ColumnStore",
-    "MariaDB Corporation",
-    "ColumnStore storage engine",
-    PLUGIN_LICENSE_GPL,
-    columnstore_init_func,
-    columnstore_done_func,
-    MCSVERSIONHEX,
-    mcs_status_variables,          /* status variables                */
-    mcs_system_variables,          /* system variables                */
-    MCSVERSION,                    /* string version */
-    COLUMNSTORE_MATURITY           /* maturity */
+  MYSQL_STORAGE_ENGINE_PLUGIN,
+  &columnstore_storage_engine,
+  "Columnstore",
+  "MariaDB",
+  "Columnstore storage engine",
+  PLUGIN_LICENSE_GPL,
+  columnstore_init_func,
+  columnstore_done_func,
+  MCSVERSIONHEX,
+  mcs_status_variables,          /* status variables                */
+  mcs_system_variables,          /* system variables                */
+  MCSVERSION,                    /* string version */
+  MariaDB_PLUGIN_MATURITY_STABLE /* maturity */
 },
 {
     MYSQL_INFORMATION_SCHEMA_PLUGIN,
@@ -960,7 +956,7 @@ maria_declare_plugin(columnstore)
     NULL,
     NULL,
     MCSVERSION,
-    COLUMNSTORE_MATURITY
+    MariaDB_PLUGIN_MATURITY_STABLE
 },
 {
     MYSQL_INFORMATION_SCHEMA_PLUGIN,
@@ -976,14 +972,14 @@ maria_declare_plugin(columnstore)
     NULL,
     NULL,
     MCSVERSION,
-    COLUMNSTORE_MATURITY
+    MariaDB_PLUGIN_MATURITY_STABLE
 },
 {
     MYSQL_INFORMATION_SCHEMA_PLUGIN,
     &is_columnstore_plugin_version,
     "COLUMNSTORE_FILES",
     "MariaDB Corporation",
-    "An information schema plugin to list ColumnStore files",
+    "An information schema plugin to list ColumnStore filess",
     PLUGIN_LICENSE_GPL,
     is_columnstore_files_plugin_init,
     //is_columnstore_files_plugin_deinit,
@@ -992,7 +988,7 @@ maria_declare_plugin(columnstore)
     NULL,
     NULL,
     MCSVERSION,
-    COLUMNSTORE_MATURITY
+    MariaDB_PLUGIN_MATURITY_STABLE
 },
 {
     MYSQL_INFORMATION_SCHEMA_PLUGIN,
@@ -1008,7 +1004,7 @@ maria_declare_plugin(columnstore)
     NULL,
     NULL,
     MCSVERSION,
-    COLUMNSTORE_MATURITY
+    MariaDB_PLUGIN_MATURITY_STABLE
 }
 maria_declare_plugin_end;
 
