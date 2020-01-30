b848d49693af23d11a132bbf38b81e9fe1bb4cad
drrtuy <drrtuy@gmail.com>
2020-01-30 17:27:36 +0300

    MCOL-641 sum() now works with DECIMAL(38) columns.
    TupleAggregateStep class method and buildAggregateColumn() now properly set result data type.
    
    doSum() now handles DECIMAL(38) in approprate manner.
    
    Low-level null related methods for new binary-based datatypes now handles magic values for
    binary-based DT.

--- dbcon/joblist/tupleaggregatestep.cpp
+++ dbcon/joblist/tupleaggregatestep.cpp
@@ -3097,12 +3097,28 @@ void TupleAggregateStep::prep2PhasesAggregate(
                         throw IDBExcept(emsg, ERR_AGGREGATE_TYPE_NOT_SUPPORT);
                     }
 
-                    oidsAggPm.push_back(oidsProj[colProj]);
-                    keysAggPm.push_back(aggKey);
-                    typeAggPm.push_back(CalpontSystemCatalog::LONGDOUBLE);
-                    scaleAggPm.push_back(0);
-                    precisionAggPm.push_back(-1);
-                    widthAggPm.push_back(sizeof(long double));
+                    // WIP MCOL-641 Replace condition with a
+                    // dynamic one
+                    if (typeProj[colProj] == CalpontSystemCatalog::DECIMAL
+                        && width[colProj] == 16)
+                    { 
+                        oidsAggPm.push_back(oidsProj[colProj]);
+                        keysAggPm.push_back(aggKey);
+                        typeAggPm.push_back(CalpontSystemCatalog::DECIMAL);
+                        scaleAggPm.push_back(0);
+                        // WIP makes this dynamic
+                        precisionAggPm.push_back(38);
+                        widthAggPm.push_back(width[colProj]);
+                    }
+                    else
+                    {
+                        oidsAggPm.push_back(oidsProj[colProj]);
+                        keysAggPm.push_back(aggKey);
+                        typeAggPm.push_back(CalpontSystemCatalog::LONGDOUBLE);
+                        scaleAggPm.push_back(0);
+                        precisionAggPm.push_back(-1);
+                        widthAggPm.push_back(sizeof(long double));
+                    }
                     colAggPm++;
                 }
 
