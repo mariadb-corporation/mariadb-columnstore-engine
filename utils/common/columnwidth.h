93c0d9a57eddbbbe1f5a85b12179cb6793f3d373
drrtuy <drrtuy@gmail.com>
2020-02-18 11:55:51 +0300

    MCOL-641 This commit adds support for SIGNED and ZEROFILL keywords in CREATE TABLE. ZEROFILL is dummy though.
    There is a new file with column width utilities.
    
    Array access was replaced by a variable that is calculated only once in
    TupleJoiner::updateCPData.

---
+++ utils/common/columnwidth.h
@@ -0,0 +1,74 @@
+/* Copyright (C) 2020 MariaDB Corporation 
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; version 2 of
+   the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+   MA 02110-1301, USA. */
+
+#ifndef UTILS_COLWIDTH_H
+#define UTILS_COLWIDTH_H
+
+#define MAXLEGACYWIDTH 8
+
+namespace utils
+{
+    inline bool isWide(uint8_t width)
+    {
+        return width > MAXLEGACYWIDTH;
+    }
+
+    inline bool isNarrow(uint8_t width)
+    {
+        return width <= MAXLEGACYWIDTH;
+    }
+
+    // WIP MCOL-641 Replace with template
+    /** @brief Map a DECIMAL precision to data width in bytes */
+    inline uint8_t widthByPrecision(unsigned p)
+    {
+        switch (p)
+        {
+            case 1:
+            case 2:
+                return 1;
+
+            case 3:
+            case 4:
+                return 2;
+
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+                return 4;
+            
+            case 10:
+            case 11:
+            case 12:
+            case 13:
+            case 14:
+            case 15:
+            case 16:
+            case 17:
+            case 18:
+                return 8;
+            
+            default:
+            return 16;
+        }
+    }
+
+}
+
+#endif // UTILS_COLWIDTH_H
