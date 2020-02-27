d585c680d4c2fe0e56863d402b53efb3cc3303e4
benthompson15 <ben.thompson.015@gmail.com>
2020-02-27 01:28:37 -0600

    Merge pull request #1072 from mariadb-corporation/fix-sm-saverpm
    1.4.2 columnstore-pre-uninstall missing this or storagemanager.cnf is lost

--- build/preInstall_platform.sh
+++ build/preInstall_platform.sh
@@ -2,6 +2,7 @@
 if [ "$1" == "2" ]; then
     #Perform tasks to prepare for the upgrade installation
     mcsadmin shutdown y > /dev/null 2>&1
+    /bin/cp -f /etc/columnstore/storagemanager.cnf /etc/columnstore/storagemanager.cnf.rpmsave > /dev/null 2>&1
     columnstore-pre-uninstall
 fi
 exit 0
