03a2283bac8ab7ce05a644781e94650cb6e18c12
Patrick LeBlanc <patrick.leblanc@mariadb.com>
2020-02-28 17:11:13 -0500

    Fixed a merge error.

--- dbcon/mysql/ha_mcs.cpp
+++ dbcon/mysql/ha_mcs.cpp
@@ -149,7 +149,6 @@ static int columnstore_init_func(void* p)
     (void) my_hash_init(&mcs_open_tables, system_charset_info, 32, 0, 0,
                         (my_hash_get_key) mcs_get_key, 0, 0);
 
-    mcs_hton->state =   SHOW_OPTION_YES;
     mcs_hton->create =  mcs_create_handler;
     mcs_hton->flags =   HTON_CAN_RECREATE;
 //  mcs_hton->discover_table = mcs_discover;
