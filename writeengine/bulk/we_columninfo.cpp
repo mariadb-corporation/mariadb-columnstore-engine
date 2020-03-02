dbf4b916fc14a38933bfe895a3fd4fa5c0da3c06
Gagan Goel <gagan.nith@gmail.com>
2020-03-02 13:23:07 -0500

    MCOL-641 Refactor empty value implementation in writeengine.

--- writeengine/bulk/we_columninfo.cpp
+++ writeengine/bulk/we_columninfo.cpp
@@ -895,7 +895,7 @@ int ColumnInfo::extendColumnOldExtent(
         }
 
         rc = colOp->expandAbbrevColumnExtent( pFile, dbRootNext,
-                                              column.emptyVal, column.width);
+                                              (uint8_t*)&column.emptyVal, column.width);
 
         if (rc != NO_ERROR)
         {
--- writeengine/wrapper/we_colop.h
+++ writeengine/wrapper/we_colop.h
@@ -220,7 +220,7 @@ public:
     /**
      * @brief Check whether it is an empty row
      */
-    EXPORT virtual bool isEmptyRow(uint64_t* curVal, uint64_t emptyVal, const int colWidth);
+    EXPORT virtual bool isEmptyRow(uint64_t* curVal, uint8_t* emptyVal, const int colWidth);
 
     /**
      * @brief Check whether it is a valid column
