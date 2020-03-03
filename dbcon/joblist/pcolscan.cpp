1f153a90e9c685c5a88e72f2f7df5804b84e4b70
Gagan Goel <gagan.nith@gmail.com>
2020-03-03 11:14:21 -0500

    MCOL-641 1. Minor refactoring of decimalToString for int128_t. 2. Update unit tests for decimalToString. 3. Allow support for wide decimal in TupleConstantStep::fillInConstants().

--- dbcon/joblist/pcolscan.cpp
+++ dbcon/joblist/pcolscan.cpp
@@ -176,7 +176,8 @@ pColScanStep::pColScanStep(
     // MCOL-641 WIP
     else if (fColType.colWidth > 8 
         && fColType.colDataType != CalpontSystemCatalog::BINARY
-        && fColType.colDataType != CalpontSystemCatalog::DECIMAL)
+        && fColType.colDataType != CalpontSystemCatalog::DECIMAL
+        && fColType.colDataType != CalpontSystemCatalog::UDECIMAL)
     {
         fColType.colWidth = 8;
         fIsDict = true;
