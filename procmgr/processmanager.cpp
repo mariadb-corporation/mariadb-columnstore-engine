7e39bb038a81a3d26616d66e3fd17ac5a6b38891
benthompson15 <ben.thompson.015@gmail.com>
2020-04-20 03:14:48 -0500

    Have process reinits wait for DMLProc to not be in BUSY_INIT state after starting. Restart ControllerNode on Primary failover only.

--- procmgr/processmanager.cpp
+++ procmgr/processmanager.cpp
@@ -3751,6 +3751,8 @@ int ProcessManager::disableModule(string target, bool manualFlag)
 
 void ProcessManager::reinitProcesses()
 {
+    Oam oam;
+
     log.writeLog(__LINE__, "reinitProcesses... ", LOG_TYPE_DEBUG);
 
     reinitProcessType("DBRMWorkerNode");
@@ -3760,7 +3762,30 @@ void ProcessManager::reinitProcesses()
     restartProcessType("DDLProc");
     sleep(1);
     restartProcessType("DMLProc");
+    sleep(1);
 
+    // waiting until dml are ACTIVE
+    while (true)
+    {
+        ProcessStatus DMLprocessstatus;
+
+        try
+        {
+            oam.getProcessStatus("DMLProc", config.moduleName(), DMLprocessstatus);
+        }
+        catch (exception& ex)
+        {}
+        catch (...)
+        {}
+
+        if (DMLprocessstatus.ProcessOpState == oam::BUSY_INIT)
+            log.writeLog(__LINE__, "Waiting for DMLProc to finish rollback", LOG_TYPE_DEBUG);
+        else
+            break;
+
+        // wait some more
+        sleep(2);
+    }
 
     log.writeLog(__LINE__, "reinitProcesses complete", LOG_TYPE_DEBUG);
 }
@@ -10503,6 +10528,8 @@ int ProcessManager::OAMParentModuleChange()
         oam::DeviceNetworkList devicenetworklist;
         processManager.setMySQLReplication(devicenetworklist, config.moduleName());
     }
+
+    processManager.restartProcessType("DBRMControllerNode");
 
     processManager.reinitProcesses();
 
