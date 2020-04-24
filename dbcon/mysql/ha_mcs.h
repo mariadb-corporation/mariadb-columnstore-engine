43d5d511d7f6791c0c3514c94c7452bd60772deb
Gagan Goel <gagan.nith@gmail.com>
2020-04-24 15:13:32 -0400

    Initial port of Monty's code of the ColumnStore cache.

--- dbcon/mysql/ha_mcs.h
+++ dbcon/mysql/ha_mcs.h
@@ -22,6 +22,7 @@
 #include <my_config.h>
 #include "idb_mysql.h"
 #include "ha_mcs_sysvars.h"
+#include "../../../maria/ha_maria.h"
 
 extern handlerton* mcs_hton;
 
@@ -234,5 +235,61 @@ public:
         return HA_CACHE_TBL_NOCACHE;
     }
 
+    int repair(THD* thd, HA_CHECK_OPT* check_opt);
+    bool is_crashed() const;
+    friend class ha_mcs_cache;
 };
+
+
+class ha_mcs_cache :public ha_mcs
+{
+  typedef ha_mcs parent;
+  int original_lock_type;
+  bool insert_command;
+
+public:
+  THR_LOCK org_lock;
+  uint lock_counter;
+  ha_maria *cache_handler;
+
+  ha_mcs_cache(handlerton *hton, TABLE_SHARE *table_arg, MEM_ROOT *mem_root);
+  ~ha_mcs_cache();
+
+  /*
+    The following functions duplicates calls to derived handler and
+    cache handler
+  */
+
+  int create(const char *name, TABLE *table_arg,
+             HA_CREATE_INFO *ha_create_info);
+  int open(const char *name, int mode, uint open_flags);
+  int delete_table(const char *name);
+  int rename_table(const char *from, const char *to);
+  int delete_all_rows(void);
+  int close(void);
+
+  uint lock_count(void) const;
+  THR_LOCK_DATA **store_lock(THD *thd,
+                             THR_LOCK_DATA **to,
+                             enum thr_lock_type lock_type);
+  int external_lock(THD *thd, int lock_type);
+  int repair(THD *thd, HA_CHECK_OPT *check_opt);
+  bool is_crashed() const;
+
+  /*
+    Write row uses cache_handler, for normal inserts, otherwise derived
+    handler
+  */
+  int write_row(const uchar *buf);
+  void start_bulk_insert(ha_rows rows, uint flags);
+  int end_bulk_insert();
+
+  /* Cache functions */
+  void free_locks();
+  bool rows_cached();
+  int flush_insert_cache();
+  friend my_bool get_status_and_flush_cache(void *param,
+                                            my_bool concurrent_insert);
+};
+
 #endif //HA_MCS_H__
