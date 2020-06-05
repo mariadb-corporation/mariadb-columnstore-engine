ea1809dc1f3bebc2ffabfb4154e857f08989c7bc
Roman Nozdrin <roman.nozdrin@mariadb.com>
2020-06-05 11:47:05 +0000

    MCOL-3914 Changed start/stop sequence to align it with the underlying dynamic systemd start order.

--- oam/install_scripts/mariadb-columnstore-start.sh
+++ oam/install_scripts/mariadb-columnstore-start.sh
@@ -2,13 +2,13 @@
 
 # This script allows to gracefully start MCS
 
+/bin/systemctl start mcs-storagemanager
 /bin/systemctl start mcs-loadbrm
 /bin/systemctl start mcs-workernode
 /bin/systemctl start mcs-controllernode
-/bin/systemctl start mcs-exemgr
-/bin/systemctl start mcs-storagemanager
-/bin/systemctl start mcs-writeengineserver
 /bin/systemctl start mcs-primproc
+/bin/systemctl start mcs-writeengineserver
+/bin/systemctl start mcs-exemgr
 /bin/systemctl start mcs-dmlproc
 /bin/systemctl start mcs-ddlproc
 
--- oam/install_scripts/mariadb-columnstore-stop.sh
+++ oam/install_scripts/mariadb-columnstore-stop.sh
@@ -4,11 +4,11 @@
 
 /bin/systemctl stop mcs-dmlproc
 /bin/systemctl stop mcs-ddlproc
-/bin/systemctl stop mcs-primproc
-/bin/systemctl stop mcs-writeengineserver
-/bin/systemctl stop mcs-storagemanager
 /bin/systemctl stop mcs-exemgr
+/bin/systemctl stop mcs-writeengineserver
+/bin/systemctl stop mcs-primproc
 /bin/systemctl stop mcs-controllernode
 /bin/systemctl stop mcs-workernode
+/bin/systemctl stop mcs-storagemanager
 
 exit 0
