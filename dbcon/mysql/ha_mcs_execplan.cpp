e02e4f9f67fc018f23991c6d59605b1c5947b1c6
David Hall <mcfadden7586@gmail.com>
2020-08-18 14:14:47 -0500

    MCOL-3827 Optimize out sort on SubQuery in Select

--- dbcon/mysql/ha_mcs_execplan.cpp
+++ dbcon/mysql/ha_mcs_execplan.cpp
@@ -7790,10 +7790,16 @@ int getSelectPlan(gp_walk_info& gwi, SELECT_LEX& select_lex,
                     }
                 }
 
-                if (ordercol->direction == ORDER::ORDER_ASC)
-                    rc->asc(true);
-                else
-                    rc->asc(false);
+                SimpleColumn* sc = dynamic_cast<SimpleColumn*>(rc);
+                if (!sc || sc->oid())
+                {
+                    if (ordercol->direction == ORDER::ORDER_ASC)
+                        rc->asc(true);
+                    else
+                        rc->asc(false);
+
+                    gwi.orderByCols.push_back(SRCP(rc));
+                }
 
                 gwi.orderByCols.push_back(SRCP(rc));
             }
@@ -9581,12 +9587,16 @@ int getGroupPlan(gp_walk_info& gwi, SELECT_LEX& select_lex, SCSEP& csep, cal_gro
                 }
             }
 
-            if (ordercol->direction == ORDER::ORDER_ASC)
-                rc->asc(true);
-            else
-                rc->asc(false);
+            SimpleColumn* sc = dynamic_cast<SimpleColumn*>(rc);
+            if (!sc || sc->oid())
+            {
+                if (ordercol->direction == ORDER::ORDER_ASC)
+                    rc->asc(true);
+                else
+                    rc->asc(false);
 
-            gwi.orderByCols.push_back(SRCP(rc));
+                gwi.orderByCols.push_back(SRCP(rc));
+            }
         }
 
 
