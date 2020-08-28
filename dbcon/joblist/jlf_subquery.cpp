9df66c4d3e37a0ebcf8e69cb3611323b464417d2
David Hall <mcfadden7586@gmail.com>
2020-08-28 13:55:34 -0500

    MCOL-3827 eid is 4 bytes

--- dbcon/joblist/jlf_subquery.cpp
+++ dbcon/joblist/jlf_subquery.cpp
@@ -823,7 +823,7 @@ void addOrderByAndLimit(CalpontSelectExecutionPlan* csep, JobInfo& jobInfo)
                     if (sc == NULL)
                     {
                         const ReturnedColumn* rc = dynamic_cast<const ReturnedColumn*>(orderByCols[i].get());
-                        uint64_t eid = rc->expressionId();
+                        uint32_t eid = rc->expressionId();
                         // If eid is -1, then there's no corresponding
                         // entry in tupleKeyMap and it will assert down the line
                         // Don't add the order by. It won't work and ordering on
