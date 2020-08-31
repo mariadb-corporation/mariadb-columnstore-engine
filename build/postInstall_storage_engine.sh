768e2fca11a2ea8779f0d980a16f656fed2ab502
benthompson15 <ben.thompson.015@gmail.com>
2020-08-31 18:13:10 -0500

    Update the package scripts, unneeded arguements passed to columnstore-post-install

--- build/postInstall_storage_engine.sh
+++ build/postInstall_storage_engine.sh
@@ -1,10 +1,5 @@
-rpmmode=install
-if  [ "$1" -eq "$1" 2> /dev/null ]; then
-	if [ $1 -ne 1 ]; then
-		rpmmode=upgrade
-	fi
-fi
 
 mkdir -p /var/lib/columnstore/local
-columnstore-post-install --rpmmode=$rpmmode
+columnstore-post-install 
 
+exit 0
--- build/preInstall_storage_engine.sh
+++ build/preInstall_storage_engine.sh
@@ -1,6 +1,6 @@
 
 if [ "$1" == "2" ]; then
-    /bin/cp -f /etc/columnstore/storagemanager.cnf /etc/columnstore/storagemanager.cnf.rpmsave > /dev/null 2>&1
     columnstore-pre-uninstall
 fi
+
 exit 0
