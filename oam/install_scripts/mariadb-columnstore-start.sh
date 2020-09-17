e332eef11875e81b2db467884ebf40546e29b9d3
Jose Rojas <jmrojas2332@gmail.com>
2020-09-17 16:28:40 -0500

    MCOL-4314 Shared storage manager cluster initialization bug fix

--- oam/install_scripts/mariadb-columnstore-start.sh
+++ oam/install_scripts/mariadb-columnstore-start.sh
@@ -2,6 +2,8 @@
 
 # This script allows to gracefully start MCS
 
+IFLAG=/var/lib/columnstore/storagemanager/cs-initialized
+
 /bin/systemctl start mcs-workernode
 /bin/systemctl start mcs-controllernode
 /bin/systemctl start mcs-primproc
@@ -9,7 +11,12 @@
 /bin/systemctl start mcs-exemgr
 /bin/systemctl start mcs-dmlproc
 /bin/systemctl start mcs-ddlproc
+sleep 2
 
-su -s /bin/sh -c 'dbbuilder 7' mysql 2> /tmp/columnstore_tmp_files/dbbuilder.log
+# prevent nodes using shared storage manager from stepping on each other when starting up
+if [ ! -e $IFLAG ]; then
+    su -s /bin/sh -c 'dbbuilder 7' mysql 2> /tmp/columnstore_tmp_files/dbbuilder.log
+    touch $IFLAG
+fi
 
 exit 0
