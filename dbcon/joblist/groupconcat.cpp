a50fa7f502b3b69a01e75586ed279db3a406fc74
Gagan Goel <gagan.nith@gmail.com>
2020-10-14 13:12:05 -0400

    MCOL-641 This commit fixes regression introduced by an earlier commit for group_concat() on a narrow decimal field.

--- dbcon/joblist/groupconcat.cpp
+++ dbcon/joblist/groupconcat.cpp
@@ -471,16 +471,15 @@ void GroupConcator::outputRow(std::ostringstream& oss, const rowgroup::Row& row)
                 else
                 {
                     int64_t intVal = row.getIntField(*i);
+
                     if (scale == 0)
                     {
                         oss << intVal;
                     }
                     else
                     {
-                        char buf[utils::MAXLENGTH8BYTES];
-                        dataconvert::DataConvert::decimalToString(intVal,
-                             scale, buf, sizeof(buf), types[*i]);
-                        oss << fixed << buf;
+                        long double dblVal = intVal / pow(10.0, (double)scale);
+                        oss << fixed << setprecision(scale) << dblVal;
                     }
                 }
 
