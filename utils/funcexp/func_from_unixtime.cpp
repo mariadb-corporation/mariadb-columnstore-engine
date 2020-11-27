d5eb68f989f5b6fa285a9775dbad3373a809d6ba
Alexander Barkov <bar@mariadb.com>
2020-11-27 14:55:45 +0400

    MCOL-4416 FROM_UNIXTIME() returns a bad result for DECIMAL input

--- utils/funcexp/func_from_unixtime.cpp
+++ utils/funcexp/func_from_unixtime.cpp
@@ -41,55 +41,103 @@ namespace
 {
 using namespace funcexp;
 
+class TUSec6
+{
+protected:
+    uint64_t  mSec;       // The integer part, between 0 and LONGLONG_MAX
+    uint32_t  mUsec;      // The fractional part, between 0 and 999999
+public:
+    explicit TUSec6(uint64_t sec, uint32_t usec = 0)
+        :mSec(sec), mUsec(usec)
+    { }
+
+    static TUSec6 fromDecimal(uint64_t val, uint8_t scale)
+    {
+        if (scale == 0)
+            return TUSec6(val);
+        if (scale <= 6)  // SSSuuu -> SSSuuuu00
+        {
+            uint64_t k = IDB_pow[scale];
+            return TUSec6(val / k, (val % k) * IDB_pow[6-scale]);
+        }
+        // scale>6 :  SSSuuuuuunnnn -> SSSuuuuuu
+        val /= IDB_pow[scale-6];
+        return TUSec6(val / 1000000, val % 1000000);
+    }
+
+    DateTime from_unixtime() const
+    {
+        if (mSec > helpers::TIMESTAMP_MAX_VALUE)
+            return 0;
+
+        DateTime dt;
+
+        struct tm tmp_tm;
+        time_t tmp_t = (time_t) mSec;
+        localtime_r(&tmp_t, &tmp_tm);
+
+        //to->neg=0;
+        dt.year = (int64_t) ((tmp_tm.tm_year + 1900) % 10000);
+        dt.month = (int64_t) tmp_tm.tm_mon + 1;
+        dt.day = (int64_t) tmp_tm.tm_mday;
+        dt.hour = (int64_t) tmp_tm.tm_hour;
+        dt.minute = (int64_t) tmp_tm.tm_min;
+        dt.second = (int64_t) tmp_tm.tm_sec;
+        dt.msecond = mUsec;
+        return dt;
+    }
+};
+
+
+class TSSec6: public TUSec6
+{
+    bool mNeg;       // false if positive, true of negative
+public:
+
+    TSSec6(bool neg, const TUSec6 &sec)
+       :TUSec6(sec), mNeg(neg)
+    { }
+    static TSSec6 fromDecimal(int64_t val, uint8_t scale)
+    {
+      if (val >= 0)
+          return TSSec6(false, TUSec6::fromDecimal((uint64_t) val, scale));
+      if (val == (int64_t) 0x8000000000000000LL) // Avoid UDB
+          return TSSec6(true, TUSec6(0x8000000000000000ULL));
+      return TSSec6(true, TUSec6((uint64_t) -val, scale));
+    }
+    DateTime from_unixtime() const
+    {
+        if (mNeg)
+            return 0;
+        return TUSec6::from_unixtime();
+    }
+};
+
+
 DateTime getDateTime(rowgroup::Row& row,
                      FunctionParm& parm,
                      bool& isNull)
 {
-    int64_t val = 0;
-    uint32_t msec = 0;
-
     switch (parm[0]->data()->resultType().colDataType)
     {
         case execplan::CalpontSystemCatalog::FLOAT:
         case execplan::CalpontSystemCatalog::DOUBLE:
         {
             double value = parm[0]->data()->getDoubleVal(row, isNull);
-            double fracpart, intpart;
-            fracpart = modf(value, &intpart);
-            val = (int64_t)intpart;
-            msec = (uint32_t)(fracpart * IDB_pow[parm[0]->data()->resultType().scale]);
-            break;
+            return TSSec6::fromDecimal((int64_t) (value * 1000000), 6).from_unixtime();
         }
         case execplan::CalpontSystemCatalog::DECIMAL:
         {
             IDB_Decimal dec = parm[0]->data()->getDecimalVal(row, isNull);
-            val = dec.value / IDB_pow[dec.scale];
-            msec = dec.value % IDB_pow[dec.scale];
-            break;
+            return TSSec6::fromDecimal((int64_t) dec.value, dec.scale).from_unixtime();
         }
 
         default:
-            val = parm[0]->data()->getDatetimeIntVal(row, isNull);
+        {
+            int64_t val = parm[0]->data()->getDatetimeIntVal(row, isNull);
+            return TSSec6::fromDecimal((int64_t) val, 0).from_unixtime();
+        }
     }
-
-    if (val < 0 || val > helpers::TIMESTAMP_MAX_VALUE)
-        return 0;
-
-    DateTime dt;
-
-    struct tm tmp_tm;
-    time_t tmp_t = (time_t)val;
-    localtime_r(&tmp_t, &tmp_tm);
-
-    //to->neg=0;
-    dt.year = (int64_t) ((tmp_tm.tm_year + 1900) % 10000);
-    dt.month = (int64_t) tmp_tm.tm_mon + 1;
-    dt.day = (int64_t) tmp_tm.tm_mday;
-    dt.hour = (int64_t) tmp_tm.tm_hour;
-    dt.minute = (int64_t) tmp_tm.tm_min;
-    dt.second = (int64_t) tmp_tm.tm_sec;
-    dt.msecond = msec;
-    return dt;
 }
 }
 
