ab6d26c27f82bde8fdff1d372c656d2c51057d26
Leonid Fedorov <leonid.fedorov@mariadb.com>
2021-08-17 15:41:57 +0000

    Has data for rowGroup

--- dbcon/joblist/windowfunctionstep.cpp
+++ dbcon/joblist/windowfunctionstep.cpp
@@ -1096,7 +1096,7 @@ void WindowFunctionStep::doPostProcessForSelect()
 
     for (int64_t i = begin; i < end; i++)
     {
-        if (rgData.hasData())
+        if (!rgData.hasData())
         {
             rgCapacity = ((rowsLeft > 8192) ? 8192 : rowsLeft);
             rowsLeft -= rgCapacity;
