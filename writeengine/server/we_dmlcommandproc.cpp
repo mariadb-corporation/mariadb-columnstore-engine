b5b5c596fb2f25c72bcaf08ee0e6a83af2d61ce7
Serguey Zefirov <serguey.zefirov@mariadb.com>
2022-04-04 18:44:10 +0300

    Fix build

--- writeengine/server/we_dmlcommandproc.cpp
+++ writeengine/server/we_dmlcommandproc.cpp
@@ -1701,7 +1701,7 @@ uint8_t WE_DMLCommandProc::processBatchInsertBinary(messageqcpp::ByteStream& bs,
 
             if (tmpStr.length() > (unsigned int)colType.colWidth)
             {
-              tmpStrassign(tmpStr.unsafeStringRef().substr(0, colType.colWidth));
+              tmpStr.assign(tmpStr.unsafeStringRef().substr(0, colType.colWidth));
 
               if (!pushWarning)
                 pushWarning = true;
