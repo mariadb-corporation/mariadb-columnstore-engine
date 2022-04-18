289b409ec0d72eeb77247e674bd7e8660a09f2f7
Leonid Fedorov <leonid.fedorov@mariadb.com>
2022-04-18 16:16:45 +0000

    Use external boost 1.78

--- cmake/boost.cmake
+++ cmake/boost.cmake
@@ -3,9 +3,11 @@
 
 
 configure_file(${CMAKE_CURRENT_LIST_DIR}/boost.CMakeLists.txt.in ${CMAKE_CURRENT_BINARY_DIR}/.boost/CMakeLists.txt @ONLY)
+SET(BOOST_ROOT ${CMAKE_CURRENT_BINARY_DIR}/.boost/boost-lib)
 
+message ("-- External: Configuring Boost")
 execute_process(
-	COMMAND ${CMAKE_COMMAND} . 
+	COMMAND ${CMAKE_COMMAND} .
     -G "${CMAKE_GENERATOR}"
     -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
     -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
@@ -17,6 +19,7 @@ if(${_exec_ret})
   message(FATAL_ERROR "Error ${_exec_ret} configuring boost dependency.")
 endif()
 
+message ("-- External: Building Boost")
 execute_process(
 	COMMAND ${CMAKE_COMMAND} --build .
 	WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/.boost
@@ -29,6 +32,13 @@ endif()
 
 unset(_exec_ret)
 
-set(BOOST_ROOT ${CMAKE_CURRENT_BINARY_DIR}/boost)
 
-find_package(Boost 1.55.0 REQUIRED COMPONENTS system filesystem thread regex date_time) 
+message("Boost installed localy at ${BOOST_ROOT}")
+
+
+SET(Boost_USE_STATIC_LIBS ON)
+FIND_PACKAGE(Boost 1.78.0 COMPONENTS system filesystem thread regex date_time chrono atomic)
+IF (NOT Boost_FOUND)
+    MESSAGE_ONCE(CS_NO_BOOST "Required Boost libraries not found!")
+    return()
+ENDIF()
