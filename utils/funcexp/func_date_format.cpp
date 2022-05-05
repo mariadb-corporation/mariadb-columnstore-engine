f7a0bbed7b6734c05f08098c69a4a7dee4b23364
Serguey Zefirov <serguey.zefirov@mariadb.com>
2022-05-05 18:52:29 +0300

    Fix build

--- utils/funcexp/func_date_format.cpp
+++ utils/funcexp/func_date_format.cpp
@@ -392,7 +392,7 @@ string Func_date_format::getStrVal(rowgroup::Row& row, FunctionParm& parm, bool&
     default: isNull = true; return "";
   }
 
-  const string& format = parm[1]->data()->getStrVal(row, isNull);
+  const string& format = parm[1]->data()->getStrVal(row, isNull).safeString("");
 
   return helpers::IDB_date_format(dt, format);
 }
