4ccbf450036d3f377c10fa9de6e8f0162d299fa5
Serguey Zefirov <serguey.zefirov@mariadb.com>
2022-05-05 20:40:36 +0300

    Fix build

--- utils/funcexp/func_unhex.cpp
+++ utils/funcexp/func_unhex.cpp
@@ -68,10 +68,12 @@ CalpontSystemCatalog::ColType Func_unhex::operationType(FunctionParm& fp,
 string Func_unhex::getStrVal(rowgroup::Row& row, FunctionParm& parm, bool& isNull,
                              CalpontSystemCatalog::ColType& op_ct)
 {
-  const auto& from = parm[0]->data()->getStrVal(row, isNull);
+  const auto& nfrom = parm[0]->data()->getStrVal(row, isNull);
 
-  if (from.isNull())
+  if (nfrom.isNull())
     return "";
+
+  const auto& from = nfrom.unsafeStringRef();
 
   char* to = new char[2 + from.size() / 2];
 
