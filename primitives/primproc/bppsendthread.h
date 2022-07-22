3b87532413a9eda06267011a7a9cd81928ca27eb
Roman Nozdrin <roman.nozdrin@mariadb.com>
2022-07-22 14:04:06 +0000

    Revert "This patch disables FairThreadPool to double check if this feature contributes to multiple strange side-effects and ocassional failed MTR tests"
    This reverts commit b78cbffa934794bbbf3f6cd5309bf481e90d6648.

--- primitives/primproc/bppsendthread.h
+++ primitives/primproc/bppsendthread.h
@@ -24,12 +24,13 @@
 
 #pragma once
 
+#include "fair_threadpool.h"
 #include "umsocketselector.h"
 #include <queue>
 #include <set>
 #include <condition_variable>
 #include "threadnaming.h"
-#include "prioritythreadpool.h"
+#include "fair_threadpool.h"
 
 namespace primitiveprocessor
 {
@@ -84,7 +85,7 @@ class BPPSendThread
   {
     return die;
   }
-  void setProcessorPool(boost::shared_ptr<threadpool::PriorityThreadPool> processorPool)
+  void setProcessorPool(boost::shared_ptr<threadpool::FairThreadPool> processorPool)
   {
     fProcessorPool = processorPool;
   }
@@ -148,7 +149,7 @@ class BPPSendThread
   uint64_t maxByteSize;
   // Used to tell the ThreadPool It should consider additional threads because a
   // queue full event has happened and a thread has been blocked.
-  boost::shared_ptr<threadpool::PriorityThreadPool> fProcessorPool;
+  boost::shared_ptr<threadpool::FairThreadPool> fProcessorPool;
 };
 
 }  // namespace primitiveprocessor
--- primitives/primproc/primitiveserver.h
+++ primitives/primproc/primitiveserver.h
@@ -36,7 +36,7 @@
 #include <boost/thread.hpp>
 
 #include "threadpool.h"
-#include "prioritythreadpool.h"
+#include "fair_threadpool.h"
 #include "messagequeue.h"
 #include "blockrequestprocessor.h"
 #include "batchprimitiveprocessor.h"
@@ -48,7 +48,6 @@ extern oam::OamCache* oamCache;
 
 namespace primitiveprocessor
 {
-extern boost::shared_ptr<threadpool::PriorityThreadPool> OOBPool;
 extern dbbc::BlockRequestProcessor** BRPp;
 extern BRM::DBRM* brm;
 extern boost::mutex bppLock;
@@ -130,14 +129,9 @@ class PrimitiveServer
 
   /** @brief get a pointer the shared processor thread pool
    */
-  inline boost::shared_ptr<threadpool::PriorityThreadPool> getProcessorThreadPool() const
+  inline boost::shared_ptr<threadpool::FairThreadPool> getProcessorThreadPool() const
   {
     return fProcessorPool;
-  }
-
-  inline boost::shared_ptr<threadpool::PriorityThreadPool> getOOBThreadPool() const
-  {
-    return fOOBPool;
   }
 
   int ReadAheadBlocks() const
@@ -171,8 +165,7 @@ class PrimitiveServer
   /** @brief the thread pool used to process
    * primitive commands
    */
-  boost::shared_ptr<threadpool::PriorityThreadPool> fProcessorPool;
-  boost::shared_ptr<threadpool::PriorityThreadPool> fOOBPool;
+  boost::shared_ptr<threadpool::FairThreadPool> fProcessorPool;
 
   int fServerThreads;
   int fServerQueueSize;
