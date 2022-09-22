815752e1075933d82a73147de733defa3c1364eb
mariadb-AndreyPiskunov <andrey.piskunov@mariadb.com>
2022-09-22 18:14:30 +0300

    Fix truncate and round for char types

--- utils/funcexp/func_truncate.cpp
+++ utils/funcexp/func_truncate.cpp
@@ -440,7 +440,7 @@ IDB_Decimal Func_truncate::getDecimalVal(Row& row, FunctionParm& parm, bool& isN
       if (!isNull)
       {
         x *= p;
-        decimal.value = (int64_t)x;
+        decimal.value = x <= INT64_MIN ? INT64_MIN : x >= INT64_MAX ? INT64_MAX : int64_t(x);
         decimal.scale = s;
       }
     }
