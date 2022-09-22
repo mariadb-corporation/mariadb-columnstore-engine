0d4f066fb4e3f2ebe6b478fabcd2aa3f3d3b393f
Roman Nozdrin <rnozdrin@mariadb.com>
2022-09-22 15:48:23 +0000

    WIP move MemManager close to RM

--- dbcon/joblist/resourcemanager.h
+++ dbcon/joblist/resourcemanager.h
@@ -120,8 +120,8 @@ class ResourceManager
   /** @brief ctor
    *
    */
-  EXPORT ResourceManager(bool runningInExeMgr = false, config::Config *aConfig = nullptr);
-  static ResourceManager* instance(bool runningInExeMgr = false, config::Config *aConfig = nullptr);
+  EXPORT ResourceManager(bool runningInExeMgr = false, config::Config* aConfig = nullptr);
+  static ResourceManager* instance(bool runningInExeMgr = false, config::Config* aConfig = nullptr);
   config::Config* getConfig()
   {
     return fConfig;
@@ -153,7 +153,7 @@ class ResourceManager
   {
     return getUintVal(fExeMgrStr, "MaxPct", defaultEMMaxPct);
   }
-  EXPORT int getEmPriority() const;   // FOr Windows only
+  EXPORT int getEmPriority() const;  // FOr Windows only
   int getEmExecQueueSize() const
   {
     return getIntVal(fExeMgrStr, "ExecQueueSize", defaultEMExecQueueSize);
@@ -561,6 +561,151 @@ inline bool ResourceManager::getBoolVal(const std::string& section, const std::s
   auto retStr = fConfig->getConfig(section, name);
   return (0 == retStr.length() ? defval : (retStr == "y" || retStr == "Y"));
 }
+
+class MemManager
+{
+ public:
+  MemManager()
+  {
+  }
+  virtual ~MemManager()
+  {
+    release(fMemUsed);
+  }
+
+  bool acquire(std::size_t amount)
+  {
+    return acquireImpl(amount);
+  }
+  void release(ssize_t amount = 0)
+  {
+    // in some cases it tries to release more memory than acquired, ie create
+    // new rowgroup, acquire maximum size (w/o strings), add some rows with
+    // strings and finally release the actual size of RG with strings
+    if (amount == 0 || amount > fMemUsed)
+      amount = fMemUsed;
+    releaseImpl(amount);
+  }
+
+  ssize_t getUsed() const
+  {
+    return fMemUsed;
+  }
+  virtual int64_t getFree() const
+  {
+    return std::numeric_limits<int64_t>::max();
+  }
+
+  virtual int64_t getConfigured() const
+  {
+    return std::numeric_limits<int64_t>::max();
+  }
+
+  virtual bool isStrict() const
+  {
+    return false;
+  }
+
+  virtual MemManager* clone() const
+  {
+    return new MemManager();
+  }
+
+  virtual joblist::ResourceManager* getResourceManaged()
+  {
+    return nullptr;
+  }
+  virtual boost::shared_ptr<int64_t> getSessionLimit()
+  {
+    return {};
+  }
+
+ protected:
+  virtual bool acquireImpl(std::size_t amount)
+  {
+    fMemUsed += amount;
+    return true;
+  }
+  virtual void releaseImpl(std::size_t amount)
+  {
+    fMemUsed -= amount;
+  }
+  ssize_t fMemUsed = 0;
+};
+
+class RMMemManager : public MemManager
+{
+ public:
+  RMMemManager(joblist::ResourceManager* rm, boost::shared_ptr<int64_t> sl, bool wait = true,
+               bool strict = true)
+   : fRm(rm), fSessLimit(std::move(sl)), fWait(wait), fStrict(strict)
+  {
+  }
+
+  ~RMMemManager() override
+  {
+    release(fMemUsed);
+    fMemUsed = 0;
+  }
+
+  int64_t getConfigured() const final
+  {
+    return fRm->getConfiguredUMMemLimit();
+  }
+
+  int64_t getFree() const final
+  {
+    return std::min(fRm->availableMemory(), *fSessLimit);
+  }
+
+  bool isStrict() const final
+  {
+    return fStrict;
+  }
+
+  MemManager* clone() const final
+  {
+    return new RMMemManager(fRm, fSessLimit, fWait, fStrict);
+  }
+
+  joblist::ResourceManager* getResourceManaged() override
+  {
+    return fRm;
+  }
+  boost::shared_ptr<int64_t> getSessionLimit() override
+  {
+    return fSessLimit;
+  }
+
+ protected:
+  bool acquireImpl(size_t amount) final
+  {
+    if (amount)
+    {
+      if (!fRm->getMemory(amount, fSessLimit, fWait) && fStrict)
+      {
+        return false;
+      }
+      MemManager::acquireImpl(amount);
+    }
+    return true;
+  }
+
+  void releaseImpl(size_t amount) override
+  {
+    if (amount)
+    {
+      MemManager::releaseImpl(amount);
+      fRm->returnMemory(amount, fSessLimit);
+    }
+  }
+
+ private:
+  joblist::ResourceManager* fRm = nullptr;
+  boost::shared_ptr<int64_t> fSessLimit;
+  const bool fWait;
+  const bool fStrict;
+};
 
 }  // namespace joblist
 
