1c5e06893709b1147945a3837b34dc07cd03514b
Serguey Zefirov <serguey.zefirov@mariadb.com>
2022-09-27 16:43:25 +0300

    Fix build

--- utils/funcexp/func_json_keys.cpp
+++ utils/funcexp/func_json_keys.cpp
@@ -54,12 +54,10 @@ CalpontSystemCatalog::ColType Func_json_keys::operationType(FunctionParm& fp,
 string Func_json_keys::getStrVal(rowgroup::Row& row, FunctionParm& fp, bool& isNull,
                                  execplan::CalpontSystemCatalog::ColType& type)
 {
-  const auto js_ns = fp[0]->data()->getStrVal(row, isNull);
+  const auto js = fp[0]->data()->getStrVal(row, isNull);
   if (isNull)
     return "";
   
-  const string_view js = js_ns.unsafeStringRef();
-
   IntType keySize = 0;
   string ret;
   json_engine_t jsEg;
