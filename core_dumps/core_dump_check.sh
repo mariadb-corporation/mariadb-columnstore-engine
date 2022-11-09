37cbd6244b46443bcabe20dc8c61bf4a2d80a650
mariadb-AndreyPiskunov <andrey.piskunov@mariadb.com>
2022-11-09 19:33:36 +0200

    Rearrange all steps + add message on failed build

--- core_dumps/core_dump_check.sh
+++ core_dumps/core_dump_check.sh
@@ -23,6 +23,7 @@ done
 for f in /$DIR_NAME/*; do
         case $f in
                 *_core_dump.*)
+                        echo $(basename ${f%%_*}) "aborted (core dumped)"
                         exit 1
                         ;;
         esac
