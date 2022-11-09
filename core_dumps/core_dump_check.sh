27e23a5c3aca3017f30220fe22e36fd53f66a924
mariadb-AndreyPiskunov <andrey.piskunov@mariadb.com>
2022-11-09 18:08:25 +0200

    Rearrange scripts and steps

--- core_dumps/core_dump_check.sh
+++ core_dumps/core_dump_check.sh
@@ -2,13 +2,28 @@
 
 DIR_NAME=$1
 RESULT=$2
+STEP_NAME=$3
 SCRIPT_LOCATION=$(dirname "$0")
+
+invoke_format ()
+{
+    file=$1
+    bash $SCRIPT_LOCATION/core_dump_format.sh $(basename ${file%%_*}) $file "$RESULT/$(basename $file.html)" "$STEP_NAME"
+}
 
 
 for f in /$DIR_NAME/*; do
         case $f in
                 *_core_dump.*)
-                        bash $SCRIPT_LOCATION/core_dump_format.sh $(basename ${f%%_*}) $f "$RESULT/$(basename $f.html)"
+                        invoke_format $f
+                        ;;
+        esac
+done
+
+for f in /$DIR_NAME/*; do
+        case $f in
+                *_core_dump.*)
+                        exit 1
                         ;;
         esac
 done
--- core_dumps/core_dump_format.sh
+++ core_dumps/core_dump_format.sh
@@ -4,6 +4,7 @@ COREDUMP=$2
 BINARY=$1
 FILENAME=$3
 SCRIPT_LOCATION=$(dirname "$0")
+STEP_NAME=$4
 
 save_ansi_to_html ()
 {
@@ -16,6 +17,7 @@ invoke_gdb_command ()
 	echo "gdb -q $BINARY --core $COREDUMP -ex '$1' -ex quit"
 }
 
+echo "<b> Step: $STEP_NAME<br> Binary name: $BINARY<br> </b>" >> $FILENAME;
 save_ansi_to_html "Backtrace" "$(invoke_gdb_command 'bt')"
 save_ansi_to_html "Arguments" "$(invoke_gdb_command 'info args')"
 save_ansi_to_html "Locals" "$(invoke_gdb_command 'info locals')"
