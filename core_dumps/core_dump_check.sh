4ee895ea91b43f838443be0107ad3f53f4fce1c4
mariadb-RomanNavrotskiy <roman.navrotskiy@mariadb.com>
2022-11-10 20:46:31 +0200

    bash quoting

--- core_dumps/core_dump_check.sh
+++ core_dumps/core_dump_check.sh
@@ -10,22 +10,22 @@ SCRIPT_LOCATION=$(dirname "$0")
 invoke_format ()
 {
     file=$1
-    bash $SCRIPT_LOCATION/core_dump_format.sh $(basename ${file%%_*}) $file "$RESULT/$(basename $file.html)" "$STEP_NAME"
+    bash "$SCRIPT_LOCATION"/core_dump_format.sh "$(basename "${file%%_*}")" "$file" "$RESULT/$(basename "$file".html)" "$STEP_NAME"
 }
 
 
-for f in /$DIR_NAME/*; do
-        case $f in
+for f in /"$DIR_NAME"/*; do
+        case "$f" in
                 *_core_dump.*)
-                        invoke_format $f
+                        invoke_format "$f"
                         ;;
         esac
 done
 
-for f in /$DIR_NAME/*; do
-        case $f in
+for f in /"$DIR_NAME"/*; do
+        case "$f" in
                 *_core_dump.*)
-                        echo $(basename ${f%%_*}) "aborted (core dumped)"
+                        echo "$(basename "${f%%_*}")" "aborted (core dumped)"
                         exit 1
                         ;;
         esac
--- core_dumps/core_dump_format.sh
+++ core_dumps/core_dump_format.sh
@@ -10,16 +10,16 @@ STEP_NAME=$4
 
 save_ansi_to_html ()
 {
-	echo "<b> $1 </b>" >> $FILENAME;
-	script -q -c "${2}" | bash $SCRIPT_LOCATION/ansi2html.sh --palette=solarized >> $FILENAME
+	echo "<b> $1 </b>" >> "${FILENAME}";
+	script -q -c "${2}" | bash "${SCRIPT_LOCATION}"/ansi2html.sh --palette=solarized >> "${FILENAME}"
 }
 
 invoke_gdb_command ()
 {
-	echo "gdb -q $BINARY --core $COREDUMP -ex '$1' -ex quit"
+	echo "gdb -q ${BINARY} --core ${COREDUMP} -ex '$1' -ex quit"
 }
 
-echo "<b> Step: $STEP_NAME<br> Binary name: $BINARY<br> </b>" >> $FILENAME;
+echo "<b> Step: ${STEP_NAME}<br> Binary name: ${BINARY}<br> </b>" >> "${FILENAME}";
 save_ansi_to_html "Backtrace" "$(invoke_gdb_command 'bt')"
 save_ansi_to_html "Arguments" "$(invoke_gdb_command 'info args')"
 save_ansi_to_html "Locals" "$(invoke_gdb_command 'info locals')"
