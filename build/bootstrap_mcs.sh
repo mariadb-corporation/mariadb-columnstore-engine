ac2f31dd07839782b2c0f25d2f7ab46976ea3b10
mariadb-AndreyPiskunov <andrey.piskunov@mariadb.com>
2022-11-14 16:36:49 +0200

    Invert build logic for core dumps

--- build/bootstrap_mcs.sh
+++ build/bootstrap_mcs.sh
@@ -28,7 +28,7 @@ optparse.define short=S long=skip-columnstore-submodules desc="Skip columnstore 
 optparse.define short=u long=skip-unit-tests desc="Skip UnitTests" variable=SKIP_UNIT_TESTS default=false value=true
 optparse.define short=B long=run-microbench="Compile and run microbenchmarks " variable=RUN_BENCHMARKS default=false value=true
 optparse.define short=b long=branch desc="Choouse git branch ('none' for menu)" variable=BRANCH
-optparse.define short=D long=with-core-dumps desc="Produce core dumps" variable=WITH_COREDUMPS default=false value=true
+optparse.define short=D long=without-core-dumps desc="Do not produce core dumps" variable=WITHOUT_COREDUMPS default=false value=true
 
 source $( optparse.build )
 
@@ -172,7 +172,7 @@ build()
         message "Buiding with unittests"
     fi
 
-    if [[ $WITH_COREDUMPS = false ]] ; then
+    if [[ $WITHOUT_COREDUMPS = true ]] ; then
         warn "Cores are not dumped"
 
     else
