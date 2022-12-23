2e52e57cc578deb0d51397c58051f3d808c763e6
Roman Nozdrin <rnozdrin@mariadb.com>
2022-12-23 20:41:07 +0000

    WIP Trivial heaporderby tests and pqorderby tests

--- dbcon/joblist/heaporderby.h
+++ dbcon/joblist/heaporderby.h
@@ -21,31 +21,98 @@
 #include <string>
 #include <vector>
 
+#include "conststring.h"
+#include "jlf_common.h"
 #include "pdqorderby.h"
 #include "resourcemanager.h"
 #include "rowgroup.h"
+#include "utils/common/vlarray.h"
 
 namespace sorting
 {
 using HeapPermutations = std::vector<PermutationVec>;
+using ColumnsValues = std::vector<utils::ConstString>;
+struct KeyType
+{
+  KeyType(rowgroup::RowGroup& rg)
+  {
+    key_ = new uint8_t(rg.getOffsets().back());
+  };
+  KeyType(rowgroup::RowGroup& rg, const joblist::OrderByKeysType& colsAndDirection,
+          const sorting::PermutationType p);
+  ~KeyType(){};
+
+  const uint8_t* key() const
+  {
+    return key_;
+  }
+  const ColumnsValues& values() const
+  {
+    return values_;
+  }
+  bool less(const KeyType& r, const rowgroup::RowGroup& rg,
+            const joblist::OrderByKeysType& colsAndDirection) const
+  {
+    assert(rg.getColumnCount() >= 1);
+    auto& widths = rg.getColWidths();
+    // auto& colTypes = rg.getColTypes();
+    rowgroup::OffsetType l_offset = 0;
+    rowgroup::OffsetType r_offset = widths.front() + 1;
+    size_t colsNumber = colsAndDirection.size();
+    // int8_t normKeyCmpResult = 0;
+    [[maybe_unused]] int32_t unsizedCmpResult = 0;
+    int32_t cmpResult = 0;
+    for (size_t i = 0; !cmpResult && i <= colsNumber; ++i)
+    {
+      if (i < colsNumber)
+      {
+        auto [colIdx, isAsc] = colsAndDirection[i];
+        if (widths[colIdx] <= 8)
+        {
+          r_offset += widths[colIdx] + 1;
+          continue;
+        }
+      }
+
+      // while ((cmpResult = memcmp(key_ + l_offset, r.key_ + l_offset, r_offset - l_offset)) == 0 /*&&
+      //      (unsizedCmpResult = CHARSET::cmp(unsizedKeys[i - 1], unsizedKeys[i])) == 0*/)
+
+      cmpResult = memcmp(key_ + l_offset, r.key_ + l_offset, r_offset - l_offset);
+      l_offset = r_offset;
+    }
+    //   if ()
+    // cmpResult = memcmp(key_ + l_offset, r.key_ + l_offset, r_offset - l_offset)) == 0
+
+    return cmpResult < 0;  //|| (cmpResult == 0 && unsizedCmpResult < 0);
+  }
+
+ private:
+  // KeyType(const uint8_t* k, ColumnsValues& values)
+  //  : key_(utils::VLArray<uint8_t, 16>(sizeof(int64_t))), values_(values){};
+  // utils::VLArray<uint8_t, 16> key_;
+  uint8_t* key_;
+  ColumnsValues values_;
+};
+
 class HeapOrderBy
 {
  private:
   using RGDataOrRowIDType = uint32_t;
-  using KeyType = int64_t;
+  // using KeyType = int64_t;
   using HeapUnit = std::pair<KeyType, PermutationType>;
   using Heap = std::vector<HeapUnit>;
   static constexpr PermutationType impossiblePermute = {0xFFFFFFFF, 0xFFFFFF, 0xFF};
 
  public:
-  HeapOrderBy(const rowgroup::RowGroup& rg, const size_t limitStart, const size_t limitCount,
-              joblist::RMMemManager* mm, const uint32_t threadId,
-              const sorting::SortingThreads& prevPhaseSorting, const uint32_t threadNum,
-              const sorting::ValueRangesVector& ranges, const joblist::OrderByKeysType& sortingKeyCols);
+  HeapOrderBy(const rowgroup::RowGroup& rg, const joblist::OrderByKeysType& sortingKeyCols,
+              const size_t limitStart, const size_t limitCount, joblist::RMMemManager* mm,
+              const uint32_t threadId, const sorting::SortingThreads& prevPhaseSorting,
+              const uint32_t threadNum, const sorting::ValueRangesVector& ranges);
   ~HeapOrderBy() = default;
   size_t getData(rowgroup::RGData& data, const SortingThreads& prevPhaseThreads);
   template <typename T>
-  size_t idxOfMin(std::vector<HeapUnit>& heap, const size_t left, const size_t right);
+  size_t idxOfMin(std::vector<HeapUnit>& heap, const size_t left, const size_t right,
+                  const joblist::OrderByKeysType& colsAndDirection);
   template <typename T>
   PermutationType getTopPermuteFromHeap(std::vector<HeapUnit>& heap, const SortingThreads& prevPhaseThreads);
   uint64_t getLimitCount() const
