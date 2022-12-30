0ce4750380562ebcb9e00eebfd1b62aa07650597
mariadb-AndreyPiskunov <andrey.piskunov@mariadb.com>
2022-12-30 17:39:15 +0200

    Tests for naive rewrite

--- dbcon/execplan/rewrites.cpp
+++ dbcon/execplan/rewrites.cpp
@@ -4,145 +4,164 @@ namespace execplan
 {
 namespace details
 {
-  template<typename T>
-  void printContainer(std::ostream& os, const T& container, const std::string& delimiter,
-                      std::function<std::string(const typename T::value_type&)> printer, const std::string& preambule = {})
+template <typename T>
+void printContainer(std::ostream& os, const T& container, const std::string& delimiter,
+                    std::function<std::string(const typename T::value_type&)> printer,
+                    const std::string& preambule = {})
+{
+  os << preambule << "\n";
+  for (auto i = container.begin(); i != container.end();)
   {
-      os << preambule << "\n";
-      for (auto i = container.begin(); i != container.end();)
-      {
-        os << printer(*i);
-        ++i;
-        if (i!= container.end())
-          os << delimiter;
-      }
-      os << std::endl;
+    os << printer(*i);
+    ++i;
+    if (i != container.end())
+      os << delimiter;
   }
+  os << std::endl;
+}
 
-  auto nodeComparator= [](const execplan::TreeNode* left, const execplan::TreeNode*right) {
-    return left->data() < right->data();
-  };
+auto nodeComparator = [](const execplan::TreeNode* left, const execplan::TreeNode* right)
+{ return left->data() < right->data(); };
 
-  using Comparator = decltype(nodeComparator);
-  using CommonContainer = std::set<execplan::TreeNode*, Comparator>;
+using Comparator = decltype(nodeComparator);
+using CommonContainer = std::set<execplan::TreeNode*, Comparator>;
 
-  //Walk the tree and find out common conjuctions
-  void collectCommonConjuctions(execplan::ParseTree* root, CommonContainer& accumulator, int level = 0) {
-    if (root == nullptr) {
-      accumulator.clear();
-      return;
-    }
-    auto sep = std::string (level * 4, '-');
-    std::cerr << sep << ": " <<  root->data()->data() << std::endl;
+// Walk the tree and find out common conjuctions
+void collectCommonConjuctions(execplan::ParseTree* root, CommonContainer& accumulator, int level = 0)
+{
+  if (root == nullptr)
+  {
+    accumulator.clear();
+    return;
+  }
+  auto sep = std::string(level * 4, '-');
+  std::cerr << sep << ": " << root->data()->data() << std::endl;
 
-    if (root->left() == nullptr && root->left() == nullptr)
-    {
-      accumulator.insert(root->data());
-      return;
-    }
-
-    if (root->data()->data() == "or") {
-      CommonContainer leftAcc;
-      CommonContainer rightAcc;
-      collectCommonConjuctions(root->left(), leftAcc, ++level);
-      collectCommonConjuctions(root->right(), rightAcc, ++level);
-      CommonContainer intersection;
-      std::set_intersection(leftAcc.begin(), leftAcc.end(),
-                    rightAcc.begin(), rightAcc.end(),
-                    std::inserter(intersection, intersection.begin()), nodeComparator);
-
-      accumulator = intersection;
-      return;
-    }
-
-    if (root->data()->data() == "and")
-    {
-      collectCommonConjuctions(root->left(), accumulator, ++level);
-      collectCommonConjuctions(root->right(), accumulator, ++level);
-      return;
-    }
-
-    if (root->left() == nullptr)
-    {
-      collectCommonConjuctions(root->right(), accumulator, ++level);
-      return;
-    }
-
-    collectCommonConjuctions(root->left(), accumulator, ++level);
+  if (root->left() == nullptr && root->left() == nullptr)
+  {
+    accumulator.insert(root->data());
     return;
   }
 
-  execplan::ParseTree* appendToRoot(execplan::ParseTree* tree, const CommonContainer& common)
+  if (root->data()->data() == "or")
   {
-      execplan::Operator* op = new execplan::Operator();
-      op->data("and");
-      execplan::ParseTree* result = new execplan::ParseTree(op);
+    CommonContainer leftAcc;
+    CommonContainer rightAcc;
+    collectCommonConjuctions(root->left(), leftAcc, ++level);
+    collectCommonConjuctions(root->right(), rightAcc, ++level);
+    CommonContainer intersection;
+    std::set_intersection(leftAcc.begin(), leftAcc.end(), rightAcc.begin(), rightAcc.end(),
+                          std::inserter(intersection, intersection.begin()), nodeComparator);
 
-      auto current = result;
-      for (auto treenode = common.begin(); treenode != common.end();)
-      {
-        execplan::Operator* op = new execplan::Operator();
-        op->data("and");
-        execplan::ParseTree* andOp = new execplan::ParseTree(op);
-        execplan::ParseTree* andCondition = new execplan::ParseTree(*treenode);
-
-        current->right(andCondition);
-        ++treenode;
-
-        if (treenode != common.end())
-        {
-          current->left(andOp);
-          current = andOp;
-        }
-      }
-      current->left(tree);
-
-      return result;
+    accumulator = intersection;
+    return;
   }
 
-  // TODO: investigate when AND has two comon nodes !!!!
-  void removeFromAnd(execplan::ParseTree* father, execplan::ParseTree* andNode, const CommonContainer& common, bool rightChild)
+  if (root->data()->data() == "and")
   {
-    if (!andNode || andNode->data()->data() != "and")
-      return;
+    collectCommonConjuctions(root->left(), accumulator, ++level);
+    collectCommonConjuctions(root->right(), accumulator, ++level);
+    return;
+  }
 
-    if (common.contains(andNode->left()->data()))
+  if (root->left() == nullptr)
+  {
+    collectCommonConjuctions(root->right(), accumulator, ++level);
+    return;
+  }
+
+  collectCommonConjuctions(root->left(), accumulator, ++level);
+  return;
+}
+
+execplan::ParseTree* appendToRoot(execplan::ParseTree* tree, const CommonContainer& common)
+{
+  execplan::Operator* op = new execplan::Operator();
+  op->data("and");
+  execplan::ParseTree* result = new execplan::ParseTree(op);
+
+  auto current = result;
+  for (auto treenode = common.begin(); treenode != common.end();)
+  {
+    execplan::Operator* op = new execplan::Operator();
+    op->data("and");
+    execplan::ParseTree* andOp = new execplan::ParseTree(op);
+    execplan::ParseTree* andCondition = new execplan::ParseTree(*treenode);
+
+    current->right(andCondition);
+    ++treenode;
+
+    if (treenode != common.end())
     {
-      if (rightChild)
-        father->right(andNode->right());
-      else
-        father->left(andNode->right());
-    }
-    else if (common.contains(andNode->right()->data()))
-    {
-      if (rightChild)
-        father->right(andNode->left());
-      else
-        father->left(andNode->left());
+      current->left(andOp);
+      current = andOp;
     }
   }
+  current->left(tree);
 
-  void removeFromTree(execplan::ParseTree* tree, const CommonContainer& common)
+  return result;
+}
+
+// TODO: investigate when AND has two comon nodes !!!!
+void removeFromAnd(execplan::ParseTree* father, execplan::ParseTree* andNode, const CommonContainer& common,
+                   bool rightChild)
+{
+  if (!andNode || andNode->data()->data() != "and")
+    return;
+
+  if (common.contains(andNode->left()->data()))
   {
-      if (tree == nullptr) {
-      return;
-    }
-
-    removeFromAnd(tree, tree->left(), common, false);
-    removeFromAnd(tree, tree->right(), common, true);
-
-    removeFromTree(tree->left(), common);
-    removeFromTree(tree->right(), common);
+    if (rightChild)
+      father->right(andNode->right());
+    else
+      father->left(andNode->right());
   }
-} // namespace details
+  else if (common.contains(andNode->right()->data()))
+  {
+    if (rightChild)
+      father->right(andNode->left());
+    else
+      father->left(andNode->left());
+  }
+}
 
+void removeFromTree(execplan::ParseTree* tree, const CommonContainer& common)
+{
+  if (tree == nullptr)
+  {
+    return;
+  }
+
+  removeFromAnd(tree, tree->left(), common, false);
+  removeFromAnd(tree, tree->right(), common, true);
+
+  removeFromTree(tree->left(), common);
+  removeFromTree(tree->right(), common);
+}
+}  // namespace details
+
+bool treeEqual(execplan::ParseTree* fst, execplan::ParseTree* snd)
+{
+  if (fst == nullptr)
+  {
+    return snd == nullptr;
+  }
+  if (snd == nullptr)
+  {
+    return fst == nullptr;
+  }
+  return fst->data() == snd->data() &&
+         ((treeEqual(fst->left(), snd->left()) && treeEqual(fst->right(), snd->right())) ||
+          (treeEqual(fst->left(), snd->right()) && treeEqual(fst->right(), snd->left())));
+}
 
 execplan::ParseTree* extractCommonLeafConjunctionsToRoot(execplan::ParseTree* tree)
 {
   details::CommonContainer common;
   details::collectCommonConjuctions(tree, common);
 
-  printContainer(std::cerr, common, "\n", [](auto treenode){ return treenode->data();}, "Common Leaf Conjunctions:");
+  printContainer(
+      std::cerr, common, "\n", [](auto treenode) { return treenode->data(); }, "Common Leaf Conjunctions:");
 
   details::removeFromTree(tree, common);
   // HACK WORKAROUND FOR case of two common nodes
@@ -150,5 +169,4 @@ execplan::ParseTree* extractCommonLeafConjunctionsToRoot(execplan::ParseTree* tr
   return details::appendToRoot(tree, common);
 }
 
-
-} // namespace execplan
+}  // namespace execplan
---
+++ tests/rewritetest2.cpp
@@ -0,0 +1,460 @@
+
+/* Copyright (C) 2022 MariaDB Corporation
+ This program is free software; you can redistribute it and/or modify it under the terms of the GNU General
+ Public License as published by the Free Software Foundation; version 2 of the License. This program is
+ distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ You should have received a copy of the GNU General Public License along with this program; if not, write to
+ the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA. */
+#include <gtest/gtest.h>
+
+#include <algorithm>
+#include <memory>
+#include "rewrites.h"
+#include "bytestream.h"
+#include "objectreader.h"
+
+#include "unit1.h"
+#include "unit10.h"
+#include "unit11.h"
+#include "unit12.h"
+#include "unit13.h"
+#include "unit14.h"
+#include "unit15.h"
+#include "unit16.h"
+#include "unit2.h"
+#include "unit3.h"
+#include "unit4.h"
+#include "unit5.h"
+#include "unit6.h"
+#include "unit7.h"
+#include "unit8.h"
+#include "unit9.h"
+
+
+using TreePtr = std::unique_ptr<execplan::ParseTree>;
+
+bool treeEqual(execplan::ParseTree* fst, execplan::ParseTree* snd)
+{
+  if (fst == nullptr)
+  {
+    return snd == nullptr;
+  }
+  if (snd == nullptr)
+  {
+    return fst == nullptr;
+  }
+  return fst->data() == snd->data() &&
+         ((treeEqual(fst->left(), snd->left()) && treeEqual(fst->right(), snd->right())) ||
+          (treeEqual(fst->left(), snd->right()) && treeEqual(fst->right(), snd->left())));
+}
+
+TEST(Simple, Check)
+{
+  messageqcpp::ByteStream stream;
+
+  /*
+
+  select t1.posname, t2.posname from t1,t2
+  where
+  (
+  t1.id = t2.id
+  and t1.pos + t2.pos < 1000
+  )
+  or
+  (
+  t1.id = t2.id
+  and t1.pos + t2.pos > 15000
+  );
+
+  */
+  stream.load(__test_query_1, sizeof(__test_query_1));
+
+  TreePtr query_1_tree;
+  query_1_tree.reset(execplan::ObjectReader::createParseTree(stream));
+  query_1_tree->drawTree("/tmp/treeq1-init.dot");
+
+  auto query_1_tree_rewritten = execplan::extractCommonLeafConjunctionsToRoot(query_1_tree.get());
+  query_1_tree_rewritten->drawTree("/tmp/treeq1-rewritten.dot");
+  EXPECT_TRUE(treeEqual(query_1_tree.get(), query_1_tree_rewritten));
+  /*
+
+  select * from t1
+  where
+  (
+  posname > 'qwer'
+  and id < 30
+  )
+  or
+  (
+  pos > 5000
+  and place > 'abcdefghij'
+  );
+
+  */
+  stream.load(__test_query_10, sizeof(__test_query_10));
+
+  TreePtr query_10_tree;
+  query_10_tree.reset(execplan::ObjectReader::createParseTree(stream));
+  query_10_tree->drawTree("/tmp/treeq10-init.dot");
+
+  auto query_10_tree_rewritten = execplan::extractCommonLeafConjunctionsToRoot(query_10_tree.get());
+  query_10_tree_rewritten->drawTree("/tmp/treeq10-rewritten.dot");
+  EXPECT_TRUE(treeEqual(query_10_tree.get(), query_10_tree_rewritten));
+  /*
+
+  select *
+  from t1
+  where
+  (
+  pos > 5000
+  and id < 30
+  )
+  or
+  (
+  pos > 5000
+  and id < 30
+  );
+
+  */
+  stream.load(__test_query_11, sizeof(__test_query_11));
+
+  TreePtr query_11_tree;
+  query_11_tree.reset(execplan::ObjectReader::createParseTree(stream));
+  query_11_tree->drawTree("/tmp/treeq11-init.dot");
+
+  auto query_11_tree_rewritten = execplan::extractCommonLeafConjunctionsToRoot(query_11_tree.get());
+  query_11_tree_rewritten->drawTree("/tmp/treeq11-rewritten.dot");
+  EXPECT_TRUE(treeEqual(query_11_tree.get(), query_11_tree_rewritten));
+  /*
+
+  select *
+  from t1
+  where
+  (
+  pos > 5000
+  and id < 30
+  )
+  and
+  (
+  pos > 5000
+  and id < 30
+  );
+
+  */
+  stream.load(__test_query_12, sizeof(__test_query_12));
+
+  TreePtr query_12_tree;
+  query_12_tree.reset(execplan::ObjectReader::createParseTree(stream));
+  query_12_tree->drawTree("/tmp/treeq12-init.dot");
+
+  auto query_12_tree_rewritten = execplan::extractCommonLeafConjunctionsToRoot(query_12_tree.get());
+  query_12_tree_rewritten->drawTree("/tmp/treeq12-rewritten.dot");
+  EXPECT_TRUE(treeEqual(query_12_tree.get(), query_12_tree_rewritten));
+  /*
+
+  select *
+  from t1
+  where
+  (
+  pos > 5000
+  or id < 30
+  )
+  or
+  (
+  pos > 5000
+  or id < 30
+  );
+
+  */
+  stream.load(__test_query_13, sizeof(__test_query_13));
+
+  TreePtr query_13_tree;
+  query_13_tree.reset(execplan::ObjectReader::createParseTree(stream));
+  query_13_tree->drawTree("/tmp/treeq13-init.dot");
+
+  auto query_13_tree_rewritten = execplan::extractCommonLeafConjunctionsToRoot(query_13_tree.get());
+  query_13_tree_rewritten->drawTree("/tmp/treeq13-rewritten.dot");
+  EXPECT_TRUE(treeEqual(query_13_tree.get(), query_13_tree_rewritten));
+  /*
+
+  select *
+  from t1
+  where
+  (
+  id in
+  (
+  select id
+  from t2
+  where posname > 'qwer'
+  and rid > 10
+  )
+  )
+  and
+  (
+  pos > 5000
+  or id < 30
+  );
+
+  */
+  stream.load(__test_query_14, sizeof(__test_query_14));
+
+  TreePtr query_14_tree;
+  query_14_tree.reset(execplan::ObjectReader::createParseTree(stream));
+  query_14_tree->drawTree("/tmp/treeq14-init.dot");
+
+  auto query_14_tree_rewritten = execplan::extractCommonLeafConjunctionsToRoot(query_14_tree.get());
+  query_14_tree_rewritten->drawTree("/tmp/treeq14-rewritten.dot");
+  EXPECT_TRUE(treeEqual(query_14_tree.get(), query_14_tree_rewritten));
+  /*
+
+  select *
+  from t1
+  where
+  (
+  id in
+  (
+  select id
+  from t2
+  where
+  (
+  posname > 'qwer'
+  and rid < 10
+  )
+  or
+  (
+  posname > 'qwer'
+  and rid > 40
+  )
+  )
+  )
+  and
+  (
+  pos > 5000
+  or id < 30);
+
+  */
+  stream.load(__test_query_15, sizeof(__test_query_15));
+
+  TreePtr query_15_tree;
+  query_15_tree.reset(execplan::ObjectReader::createParseTree(stream));
+  query_15_tree->drawTree("/tmp/treeq15-init.dot");
+
+  auto query_15_tree_rewritten = execplan::extractCommonLeafConjunctionsToRoot(query_15_tree.get());
+  query_15_tree_rewritten->drawTree("/tmp/treeq15-rewritten.dot");
+  EXPECT_TRUE(treeEqual(query_15_tree.get(), query_15_tree_rewritten));
+  /*
+
+  select *
+  from t1
+  where
+  (
+  id in
+  (
+  select id
+  from t2
+  where
+  posname > 'qwer'
+  and
+  (
+  rid < 10
+  or rid > 40
+  )
+  )
+  )
+  and
+  (
+  pos > 5000
+  or id < 30
+  );
+
+  */
+  stream.load(__test_query_16, sizeof(__test_query_16));
+
+  TreePtr query_16_tree;
+  query_16_tree.reset(execplan::ObjectReader::createParseTree(stream));
+  query_16_tree->drawTree("/tmp/treeq16-init.dot");
+
+  auto query_16_tree_rewritten = execplan::extractCommonLeafConjunctionsToRoot(query_16_tree.get());
+  query_16_tree_rewritten->drawTree("/tmp/treeq16-rewritten.dot");
+  EXPECT_TRUE(treeEqual(query_16_tree.get(), query_16_tree_rewritten));
+  /*
+
+  select t1.posname, t2.posname
+  from t1,t2
+  where
+  t1.id = t2.id
+  and (t1.pos + t2.pos < 1000);
+
+  */
+  stream.load(__test_query_2, sizeof(__test_query_2));
+
+  TreePtr query_2_tree;
+  query_2_tree.reset(execplan::ObjectReader::createParseTree(stream));
+  query_2_tree->drawTree("/tmp/treeq2-init.dot");
+
+  auto query_2_tree_rewritten = execplan::extractCommonLeafConjunctionsToRoot(query_2_tree.get());
+  query_2_tree_rewritten->drawTree("/tmp/treeq2-rewritten.dot");
+  EXPECT_TRUE(treeEqual(query_2_tree.get(), query_2_tree_rewritten));
+  /*
+
+  select t1.posname, t2.posname
+  from t1,t2
+  where
+  (t1.pos + t2.pos < 1000)
+  or
+  (t1.pos + t2.pos > 16000)
+  or
+  (t1.posname < dcba);
+
+  */
+  stream.load(__test_query_3, sizeof(__test_query_3));
+
+  TreePtr query_3_tree;
+  query_3_tree.reset(execplan::ObjectReader::createParseTree(stream));
+  query_3_tree->drawTree("/tmp/treeq3-init.dot");
+
+  auto query_3_tree_rewritten = execplan::extractCommonLeafConjunctionsToRoot(query_3_tree.get());
+  query_3_tree_rewritten->drawTree("/tmp/treeq3-rewritten.dot");
+  EXPECT_TRUE(treeEqual(query_3_tree.get(), query_3_tree_rewritten));
+  /*
+
+  select t1.posname, t2.posname
+  from t1,t2
+  where
+  (t1.pos > 20)
+  or
+  (t2.posname in (select t1.posname from t1 where t1.pos > 20));
+
+  */
+  stream.load(__test_query_4, sizeof(__test_query_4));
+
+  TreePtr query_4_tree;
+  query_4_tree.reset(execplan::ObjectReader::createParseTree(stream));
+  query_4_tree->drawTree("/tmp/treeq4-init.dot");
+
+  auto query_4_tree_rewritten = execplan::extractCommonLeafConjunctionsToRoot(query_4_tree.get());
+  query_4_tree_rewritten->drawTree("/tmp/treeq4-rewritten.dot");
+  EXPECT_TRUE(treeEqual(query_4_tree.get(), query_4_tree_rewritten));
+  /*
+
+  select t1.posname, t2.posname from t1,t2
+  where
+  (
+  t1.id = t2.id
+  or t1.pos + t2.pos < 1000
+  )
+  and
+  (
+  t1.id = t2.id
+  or t1.pos + t2.pos > 15000
+  );
+
+  */
+  stream.load(__test_query_5, sizeof(__test_query_5));
+
+  TreePtr query_5_tree;
+  query_5_tree.reset(execplan::ObjectReader::createParseTree(stream));
+  query_5_tree->drawTree("/tmp/treeq5-init.dot");
+
+  auto query_5_tree_rewritten = execplan::extractCommonLeafConjunctionsToRoot(query_5_tree.get());
+  query_5_tree_rewritten->drawTree("/tmp/treeq5-rewritten.dot");
+  EXPECT_TRUE(treeEqual(query_5_tree.get(), query_5_tree_rewritten));
+  /*
+
+  select t1.posname, t2.posname from t1,t2
+  where
+  (
+  t1.id = t2.rid
+  or t1.pos + t2.pos < 1000
+  )
+  and
+  (
+  t1.id = t2.id
+  or t1.pos + t2.pos > 15000
+  );
+
+  */
+  stream.load(__test_query_6, sizeof(__test_query_6));
+
+  TreePtr query_6_tree;
+  query_6_tree.reset(execplan::ObjectReader::createParseTree(stream));
+  query_6_tree->drawTree("/tmp/treeq6-init.dot");
+
+  auto query_6_tree_rewritten = execplan::extractCommonLeafConjunctionsToRoot(query_6_tree.get());
+  query_6_tree_rewritten->drawTree("/tmp/treeq6-rewritten.dot");
+  EXPECT_TRUE(treeEqual(query_6_tree.get(), query_6_tree_rewritten));
+  /*
+
+  select t1.posname
+  from t1
+  where
+  t1.posname in
+  (
+  select t1.posname
+  from t1
+  where posname > 'qwer'
+  and
+  id < 30
+  );
+
+  */
+  stream.load(__test_query_7, sizeof(__test_query_7));
+
+  TreePtr query_7_tree;
+  query_7_tree.reset(execplan::ObjectReader::createParseTree(stream));
+  query_7_tree->drawTree("/tmp/treeq7-init.dot");
+
+  auto query_7_tree_rewritten = execplan::extractCommonLeafConjunctionsToRoot(query_7_tree.get());
+  query_7_tree_rewritten->drawTree("/tmp/treeq7-rewritten.dot");
+  EXPECT_TRUE(treeEqual(query_7_tree.get(), query_7_tree_rewritten));
+  /*
+
+  select t1.posname, t2.posname
+  from t1,t2
+  where t1.posname in
+  (
+  select t1.posname
+  from t1
+  where posname > 'qwer'
+  and id < 30
+  )
+  and t1.id = t2.id;
+
+  */
+  stream.load(__test_query_8, sizeof(__test_query_8));
+
+  TreePtr query_8_tree;
+  query_8_tree.reset(execplan::ObjectReader::createParseTree(stream));
+  query_8_tree->drawTree("/tmp/treeq8-init.dot");
+
+  auto query_8_tree_rewritten = execplan::extractCommonLeafConjunctionsToRoot(query_8_tree.get());
+  query_8_tree_rewritten->drawTree("/tmp/treeq8-rewritten.dot");
+  EXPECT_TRUE(treeEqual(query_8_tree.get(), query_8_tree_rewritten));
+  /*
+
+  select t1.posname, t2.posname
+  from t1,t2
+  where t1.posname in
+  (
+  select t1.posname
+  from t1
+  where posname > 'qwer'
+  and id < 30
+  ) and
+  (
+  t1.id = t2.id
+  and t1.id = t2.rid
+  );
+
+  */
+  stream.load(__test_query_9, sizeof(__test_query_9));
+
+  TreePtr query_9_tree;
+  query_9_tree.reset(execplan::ObjectReader::createParseTree(stream));
+  query_9_tree->drawTree("/tmp/treeq9-init.dot");
+
+  auto query_9_tree_rewritten = execplan::extractCommonLeafConjunctionsToRoot(query_9_tree.get());
+  query_9_tree_rewritten->drawTree("/tmp/treeq9-rewritten.dot");
+  EXPECT_TRUE(treeEqual(query_9_tree.get(), query_9_tree_rewritten));
+}
