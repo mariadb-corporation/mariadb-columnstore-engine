176eefcc5756108373a7bdc209e4e1be714699c3
Roman Nozdrin <roman.nozdrin@mariadb.com>
2023-03-17 21:17:04 +0000

    MCOL-5456 codebase preparation for colab with CS 11.1

---
+++ cmake/mpark_patterns.cmake
@@ -0,0 +1,24 @@
+include(ExternalProject)
+
+if(CMAKE_CXX_COMPILER_ID MATCHES "GNU")
+  set(_toolset "gcc")
+elseif(CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
+  set(_toolset "clang")
+elseif(CMAKE_CXX_COMPILER_ID MATCHES "Intel")
+  set(_toolset "intel-linux")
+endif()
+
+# set(INSTALL_LOCATION ${CMAKE_CURRENT_BINARY_DIR}/.boost/boost-lib)
+# SET(Boost_INCLUDE_DIRS "${INSTALL_LOCATION}/include")
+# SET(Boost_LIBRARY_DIRS "${INSTALL_LOCATION}/lib")
+# LINK_DIRECTORIES("${Boost_LIBRARY_DIRS}")
+
+ExternalProject_Add(mpark_patterns
+  PREFIX external/patternes
+  URL https://github.com/mpark/patterns/archive/refs/tags/v0.3.0.tar.gz
+  URL_HASH SHA256=80e6af808a4d74d5d7358666303eb1dbfc5582313ff9fa31d1c0d3280d3bd9e7
+  BUILD_IN_SOURCE TRUE
+  LOG_BUILD TRUE
+  LOG_INSTALL TRUE
+  EXCLUDE_FROM_ALL TRUE
+)
