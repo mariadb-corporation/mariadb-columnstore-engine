b0761ee7eda99bb6d999cb1c98faee867035dfcb
Serguey Zefirov <serguey.zefirov@mariadb.com>
2023-03-24 19:30:15 +0300

    Fix build

--- primitives/primproc/primproc.cpp
+++ primitives/primproc/primproc.cpp
@@ -795,7 +795,7 @@ int main(int argc, char** argv)
   unsigned narenas = 0x900df00d;
   size_t sz = sizeof(narenas);
 
-  bool ret = mallctl("arenas.narenas", (void*)(&narenas), &sz, nullptr, nullptr);
+  bool ret = mallctl("arenas.narenas", (void*)(&narenas), &sz, nullptr, 0);
   if (ret) {
     std::cerr << "unable to get number of arenas\n";
     return 1;
@@ -812,7 +812,7 @@ int main(int argc, char** argv)
     extent_hooks_t* newHooks = &ppHooks;
     size_t szNew = sizeof(newHooks);
     size_t szOld = sizeof(ppOldArenasHooks[i]);
-    if (mallctl(tmp.str().c_str(), &ppOldArenasHooks[i], &szOld, &newHooks, &szNew))
+    if (mallctl(tmp.str().c_str(), &ppOldArenasHooks[i], &szOld, &newHooks, szNew))
     {
       std::cerr << "unable to set hooks for arena #" << i;
       return 1;
