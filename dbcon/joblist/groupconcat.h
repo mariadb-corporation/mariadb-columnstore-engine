499859035c1af97ad1c8ee6d31392735ee348390
Gagan Goel <gagan.nith@gmail.com>
2023-04-19 21:11:06 +0000

    MCOL-5429 Fix high memory consumption in GROUP_CONCAT() processing.
    1. Input and output RowGroup's used in GROUP_CONCAT classes
    are currently allocating a raw memory buffer of size equal
    to the actual width of the string datatype. As an example,
    for the following query:
      SELECT col1, GROUP_CONCAT(col2) FROM t GROUP BY col1;
    If col2 is a TEXT field with default width, the input
    RowGroup containing the target rows to be concatenated will
    assign 64kb of memory for every input row in the RowGroup.
    This is wasteful as actual field values in real workloads
    would be much smaller. We fix this by enabling the
    RowGroup to use the StringStore when the RowGroup contains
    long strings.
    
    2. RowAggregation::initialize() allocates a memory buffer
    for a NULL row. The size of this buffer is equal to the
    row size for the output RowGroup. For the above scenario,
    using the default group_concat_max_len (which is a server
    variable that sets the maximum length of the GROUP_CONCAT string)
    value of 1mb, the buffer size would be
    (1mb + 64kb + some additional metadata). If the user sets
    group_concat_max_len to a higher value, say 3gb, this buffer
    size would be ~3gb. Now if the runtime initiates several
    instances of RowAggregation, total memory consumption by
    PrimProc could exceed the hardware memory limits causing the
    OS OOM to kill the process. We fix this problem by again
    enabling the StringStore for the NULL row allocation.
    
    3. In the plugin code in buildAggregateColumn(), there is
    an integer overflow when the server group_concat_max_len
    variable (which is an uint32_t) is set to a value > INT32_MAX
    (such as 3gb) and is assigned to
    CalpontSystemCatalog::ColType::colWidth (which is an int32_t).
    As a short term fix, we saturate the assigned value to colWidth
    to INT32_MAX. Proper fix would be to upgrade
    CalpontSystemCatalog::ColType::colWidth to an uint32_t.

--- dbcon/joblist/groupconcat.h
+++ dbcon/joblist/groupconcat.h
@@ -93,6 +93,8 @@ class GroupConcatAgUM : public rowgroup::GroupConcatAg
   boost::scoped_ptr<GroupConcator> fConcator;
   boost::scoped_array<uint8_t> fData;
   rowgroup::Row fRow;
+  rowgroup::RGData fRowRGData;
+  rowgroup::RowGroup fRowGroup;
   bool fNoOrder;
 };
 
