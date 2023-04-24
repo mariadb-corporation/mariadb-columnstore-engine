addc8f6a997f1c752bc695db976048a5a468c172
Gagan Goel <gagan.nith@gmail.com>
2023-04-24 22:32:05 +0000

    cmake fixes related to mpark_patterns.

--- cmake/mpark_patterns.cmake
+++ cmake/mpark_patterns.cmake
@@ -8,13 +8,8 @@ elseif(CMAKE_CXX_COMPILER_ID MATCHES "Intel")
   set(_toolset "intel-linux")
 endif()
 
-# set(INSTALL_LOCATION ${CMAKE_CURRENT_BINARY_DIR}/.boost/boost-lib)
-# SET(Boost_INCLUDE_DIRS "${INSTALL_LOCATION}/include")
-# SET(Boost_LIBRARY_DIRS "${INSTALL_LOCATION}/lib")
-# LINK_DIRECTORIES("${Boost_LIBRARY_DIRS}")
-
 ExternalProject_Add(mpark_patterns
-  PREFIX external/patternes
+  PREFIX external/patterns
   URL https://github.com/mpark/patterns/archive/refs/tags/v0.3.0.tar.gz
   URL_HASH SHA256=80e6af808a4d74d5d7358666303eb1dbfc5582313ff9fa31d1c0d3280d3bd9e7
   BUILD_IN_SOURCE TRUE
