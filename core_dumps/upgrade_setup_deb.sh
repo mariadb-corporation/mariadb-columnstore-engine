dc9f34daa16ce590417ca6e774186b010a453708
mariadb-AndreyPiskunov <andrey.piskunov@mariadb.com>
2023-06-09 21:27:33 +0300

    Squash all

---
+++ core_dumps/upgrade_setup_deb.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -xeuo pipefail
+
+VERSION="$1"
+RESULT="$2"
+ARCH="$3"
+LINK="$4"
+UPGRADE_TOKEN="$5"
+
+sed -i "s/exit 101/exit 0/g" /usr/sbin/policy-rc.d
+bash -c "apt update --yes && apt install -y procps wget curl"
+wget https://dlm.mariadb.com/enterprise-release-helpers/mariadb_es_repo_setup -O mariadb_es_repo_setup
+chmod +x mariadb_es_repo_setup
+bash -c "./mariadb_es_repo_setup --token=${UPGRADE_TOKEN} --apply --mariadb-server-version=${VERSION} --skip-maxscale --skip-tools"
+apt update --yes
+apt install --yes mariadb-server mariadb-client mariadb-plugin-columnstore
+systemctl start mariadb
+systemctl start mariadb-columnstore
+bash -c "./upgrade_data.sh"
+bash -c "./upgrade_verify.sh"
+
+touch /etc/apt/auth.conf
+cat << EOF > /etc/apt/auth.conf
+machine ${LINK}${RESULT}/
+EOF
+
+apt update --yes
+apt install -y ca-certificates
+cd /etc/apt/sources.list.d
+echo "deb [trusted=yes] ${LINK} ${RESULT}/" > repo.list
+
+cd /
+apt update --yes
+apt upgrade --yes
+bash -c "./upgrade_verify.sh"
