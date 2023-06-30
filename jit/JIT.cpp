c5d17562e0668159632e6cd6c4dbaa76e9ab05e7
noorall <863485501@qq.com>
2023-06-30 16:43:48 +0800

    [feat] add implement of arena memory pool

--- jit/JIT.cpp
+++ jit/JIT.cpp
@@ -21,26 +21,160 @@
 #include <llvm/Support/TargetSelect.h>
 #include <llvm/Transforms/IPO/PassManagerBuilder.h>
 #include <llvm/Support/SmallVectorMemoryBuffer.h>
+#include <llvm/ExecutionEngine/RTDyldMemoryManager.h>
 
-// TODO: implement this
-class MemoryPage
+
+// Arena Memory pool for JIT
+class Arena : private boost::noncopyable
 {
  public:
-  MemoryPage() : page_size(0)
+  Arena() : page_size(8)
   {
   }
-  char* allocateMemory(size_t size, size_t alignment)
+  ~Arena()
   {
-    return nullptr;
+    protect(PROT_READ | PROT_WRITE);
+
+    for (auto& block : blocks)
+    {
+      free(block.base());
+    }
   }
+  /*
+   * param: size: the size of the memory to be allocated
+   * return: the address of the allocated memory
+   * */
+  char* allocateAligned(size_t size, size_t alignment)
+  {
+    for (size_t i = 0; i < blocks.size(); ++i)
+    {
+      char* result = allocateFromBlockWithIndex(size, alignment, i);
+      if (result)
+      {
+        return result;
+      }
+    }
+    allocateNewBlock(size);
+    size_t allocated_index = blocks.size() - 1;
+    char* result = allocateFromBlockWithIndex(size, alignment, allocated_index);
+    assert(result);
+    return result;
+  }
+
+  void protect(int flag)
+  {
+#if defined(__NetBSD__) && defined(PROT_MPROTECT)
+    flag |= PROT_MPROTECT(PROT_READ | PROT_WRITE | PROT_EXEC);
+#endif
+
+    bool invalidate_cache = (flag & PROT_EXEC);
+
+    for (const auto& block : blocks)
+    {
+#if defined(__arm__) || defined(__aarch64__)
+      /// Certain ARM implementations treat icache clear instruction as a memory read,
+      /// and CPU segfaults on trying to clear cache on !PROT_READ page.
+      /// Therefore we need to temporarily add PROT_READ for the sake of flushing the instruction caches.
+      if (invalidate_cache && !(flag & PROT_READ))
+      {
+        int res = mprotect(block.base(), block.blockSize(), flag | PROT_READ);
+        if (res != 0)
+          throw std::runtime_error("Cannot mprotect memory region");
+
+        llvm::sys::Memory::InvalidateInstructionCache(block.base(), block.blockSize());
+        invalidate_cache = false;
+      }
+#endif
+      int res = mprotect(block.base(), block.blockSize(), flag);
+      if (res != 0)
+        throw std::runtime_error("Cannot mprotect memory region");
+
+      if (invalidate_cache)
+        llvm::sys::Memory::InvalidateInstructionCache(block.base(), block.blockSize());
+    }
+  }
+
   inline size_t getAllocatedSize() const
   {
     return allocated_size;
   }
 
+  inline size_t getPageSize() const
+  {
+    return page_size;
+  }
+
  private:
   size_t page_size = 0;
   size_t allocated_size = 0;
+  struct Block
+  {
+   public:
+    Block(void* pages_base_, size_t pages_size_, size_t page_size_)
+     : pages_base(pages_base_), pages_size(pages_size_), page_size(page_size_)
+    {
+    }
+    inline void* base() const
+    {
+      return pages_base;
+    }
+    inline size_t pagesSize() const
+    {
+      return pages_size;
+    }
+    inline size_t pageSize() const
+    {
+      return page_size;
+    }
+    inline size_t blockSize() const
+    {
+      return pages_size * page_size;
+    }
+
+   private:
+    void* pages_base;
+    size_t pages_size;
+    size_t page_size;
+  };
+  std::vector<Block> blocks;
+  std::vector<size_t> blocks_allocated_size;
+
+  char* allocateFromBlockWithIndex(size_t size, size_t alignment, size_t block_index)
+  {
+    assert(block_index < blocks.size());
+    auto& block = blocks[block_index];
+
+    size_t block_size = block.blockSize();
+    size_t& block_allocated_size = blocks_allocated_size[block_index];
+    size_t block_free_size = block_size - block_allocated_size;
+
+    uint8_t* block_base = static_cast<uint8_t*>(block.base());
+    void* offset = block_base + block_allocated_size;
+    auto* result = std::align(alignment, size, offset, block_free_size);
+    if (result)
+    {
+      block_allocated_size = reinterpret_cast<uint8_t*>(result) - block_base;
+      block_allocated_size += size;
+      return static_cast<char*>(result);
+    }
+    return nullptr;
+  }
+  void allocateNewBlock(size_t size)
+  {
+    size_t allocate_page_num = ((size / page_size) + 1) * 2;
+    size_t allocate_size = page_size * allocate_page_num;
+
+    void* buf = nullptr;
+    int res = posix_memalign(&buf, page_size, allocate_size);
+    if (res != 0)
+    {
+      throw std::runtime_error("Cannot allocate memory");
+    }
+    blocks.emplace_back(buf, allocate_page_num, page_size);
+    blocks_allocated_size.emplace_back(0);
+
+    allocated_size += allocate_size;
+  }
 };
 
 class JITModuleMemoryManager : public llvm::RTDyldMemoryManager
@@ -49,18 +183,18 @@ class JITModuleMemoryManager : public llvm::RTDyldMemoryManager
   uint8_t* allocateCodeSection(uintptr_t size, unsigned alignment, unsigned section_id,
                                llvm::StringRef section_name) override
   {
-    return reinterpret_cast<uint8_t*>(exec_memory_page.allocateMemory(size, alignment));
+    return reinterpret_cast<uint8_t*>(exec_memory_page.allocateAligned(size, alignment));
   }
   uint8_t* allocateDataSection(uintptr_t size, unsigned alignment, unsigned, llvm::StringRef,
                                bool is_read_only) override
   {
     if (is_read_only)
     {
-      return reinterpret_cast<uint8_t*>(ro_memory_page.allocateMemory(size, alignment));
+      return reinterpret_cast<uint8_t*>(ro_memory_page.allocateAligned(size, alignment));
     }
     else
     {
-      return reinterpret_cast<uint8_t*>(rw_memory_page.allocateMemory(size, alignment));
+      return reinterpret_cast<uint8_t*>(rw_memory_page.allocateAligned(size, alignment));
     }
   }
 
@@ -76,9 +210,9 @@ class JITModuleMemoryManager : public llvm::RTDyldMemoryManager
   }
 
  private:
-  MemoryPage rw_memory_page;
-  MemoryPage ro_memory_page;
-  MemoryPage exec_memory_page;
+  Arena rw_memory_page;
+  Arena ro_memory_page;
+  Arena exec_memory_page;
 };
 
 class JITCompiler
