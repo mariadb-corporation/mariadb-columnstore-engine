2ae171f96c8cae537e11a0c6260a05ef7d309bf8
Leonid Fedorov <leonid.fedorov@mariadb.com>
2023-09-29 11:19:38 +0000

    chore(ci) refactor reports from tests stage, add it to upgrade

--- core_dumps/upgrade_setup_deb.sh
+++ core_dumps/upgrade_setup_deb.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+source ../build/utils.sh
+
 set -xeuo pipefail
 
 VERSION="$1"
@@ -7,6 +9,9 @@ RESULT="$2"
 ARCH="$3"
 LINK="$4"
 UPGRADE_TOKEN="$5"
+
+DEBIAN_FRONTEND=noninteractive
+UCF_FORCE_CONFNEW=1
 
 apt install --yes rsyslog
 sed -i "s/exit 101/exit 0/g" /usr/sbin/policy-rc.d
@@ -18,6 +23,9 @@ apt update --yes
 apt install --yes mariadb-server mariadb-client mariadb-plugin-columnstore
 systemctl start mariadb
 systemctl start mariadb-columnstore
+
+INITIAL_VERSION=$(mariadb -e "select @@version;")
+
 bash -c "./upgrade_data.sh"
 bash -c "./upgrade_verify.sh"
 
@@ -28,5 +36,26 @@ EOF
 
 bash -c "./setup-repo.sh"
 
-apt upgrade --yes
-bash -c "./upgrade_verify.sh"
+
+# Configuration file '/etc/columnstore/Columnstore.xml'
+#  ==> Modified (by you or by a script) since installation.
+#  ==> Package distributor has shipped an updated version.
+#    What would you like to do about it ?  Your options are:
+#     Y or I  : install the package maintainer's version
+#     N or O  : keep your currently-installed version
+#       D     : show the differences between the versions
+#       Z     : start a shell to examine the situation
+#  The default action is to keep your current version.
+
+# rm to escape halt on interactive question
+apt-get --yes --with-new-pkgs -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
+
+UPGRADED_VERSION=$(mariadb -e "select @@version;")
+
+if [[ "$INITIAL_VERSION" == "$UPGRADED_VERSION" ]]; then
+  error "The upgrade didn't happen!"
+  exit 1
+else
+  message_splitted "The upgrade from "$INITIAL_VERSION" to "$UPGRADED_VERSION" succeded!"
+  bash -c "./upgrade_verify.sh"
+fi
