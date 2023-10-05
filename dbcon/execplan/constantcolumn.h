2e1a4370d035a13ed950c0b3c1318f2a968cfb4e
Serguey Zefirov <serguey.zefirov@mariadb.com>
2023-10-05 18:09:18 +0300

    Debug logs

--- dbcon/execplan/constantcolumn.h
+++ dbcon/execplan/constantcolumn.h
@@ -430,6 +430,7 @@ class ConstantColumnUInt : public ConstantColumn
   ConstantColumnUInt(uint64_t val, int8_t scale, uint8_t precision)
    : ConstantColumn(val, ConstantColumn::NUM, scale, precision)
   {
+	  idblog("uint constructor");
   }
 };
 
@@ -439,6 +440,7 @@ class ConstantColumnSInt : public ConstantColumn
   ConstantColumnSInt(const CalpontSystemCatalog::ColType& type, const std::string& str, int64_t val)
    : ConstantColumn(str, val, ConstantColumn::NUM)
   {
+	  idblog("assigning type - this is stupid!!!");
     resultType(type);
   }
 };
