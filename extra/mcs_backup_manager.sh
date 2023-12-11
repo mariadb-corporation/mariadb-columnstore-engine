ec0c5ea0457dc6caaf1ea21ae5572aa1c0b34cae
Allen Herrera <allen.herrera@mariadb.com>
2023-12-11 12:05:17 -0500

    mcs_backup_manager.sh - typo in comments of bsl license

--- extra/mcs_backup_manager.sh
+++ extra/mcs_backup_manager.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-/*
- * Copyright (c) 2020 MariaDB Corporation Ab
- *
- * Use of this software is governed by the Business Source License included
- * in the LICENSE.TXT file and at www.mariadb.com/bsl11.
- *
- * Change Date: 2024-07-10
- *
- * On the date above, in accordance with the Business Source License, use
- * of this software will be governed by version 2 or later of the General
- * Public License.
- */
- 
+########################################################################
+# Copyright (c) 2020 MariaDB Corporation Ab
+#
+# Use of this software is governed by the Business Source License included
+# in the LICENSE.TXT file and at www.mariadb.com/bsl11.
+#
+# Change Date: 2024-07-10
+#
+# On the date above, in accordance with the Business Source License, use
+# of this software will be governed by version 2 or later of the General
+# Public License.
+#
 ########################################################################
 # Documentation:  bash mcs_backup_manager.sh help
 # Version: 3.3
