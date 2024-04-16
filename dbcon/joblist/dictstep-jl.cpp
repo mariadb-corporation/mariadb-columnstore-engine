2d555e2555c00a88693854aee4b89cce6451e039
Serguey Zefirov <serguey.zefirov@mariadb.com>
2024-04-16 13:48:17 +0300

    Possible fix

--- dbcon/joblist/dictstep-jl.cpp
+++ dbcon/joblist/dictstep-jl.cpp
@@ -233,7 +233,7 @@ messageqcpp::ByteStream DictStepJL::reencodedFilterString() const
            return !std::isspace(ch);
           }).base(), fs.end());
       }
-      encodedPrefix = encodeStringPrefix(fs.c_str(), fs.size(), cset);
+      encodedPrefix = encodeStringPrefix(reinterpret_cast<const uint8_t*>(fs.c_str()), fs.size(), cset);
 	  idblog(" op " << int(cop) << ", <<" << fs << ">>, prefix " << std::hex << encodedPrefix);
       bs << encodedPrefix;
       filterStringCopy.advance(size);
