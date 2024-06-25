7ec8f3df9acf8d59015c3ee7da2065ca51886215
Sergey Zefirov <72864488+mariadb-SergeyZefirov@users.noreply.github.com>
2024-06-25 15:10:27 +0300

    MCOL-5772: incorrect ORDER BY ordering for a columns not in GROUP BY (#3214)
    When ORDER BY column is not in GROUP BY, is not an aggregate and there
    is a SELECT column that is also not an aggregate, there was a problem:
    ordering happened on the SELECTed column, not ORDERed one.
    
    This patch fixes that particular problem and also performs some tidying
    around newly added aggregate.

--- dbcon/joblist/tupleaggregatestep.cpp
+++ dbcon/joblist/tupleaggregatestep.cpp
@@ -1770,7 +1770,7 @@ void TupleAggregateStep::prep1PhaseDistinctAggregate(JobInfo& jobInfo, vector<Ro
       {
         case ROWAGG_MIN:
         case ROWAGG_MAX:
-	case ROWAGG_SELECT_SOME:
+        case ROWAGG_SELECT_SOME:
         {
           oidsAgg.push_back(oidsProj[colProj]);
           keysAgg.push_back(aggKey);
@@ -2181,6 +2181,7 @@ void TupleAggregateStep::prep1PhaseDistinctAggregate(JobInfo& jobInfo, vector<Ro
         case ROWAGG_BIT_AND:
         case ROWAGG_BIT_OR:
         case ROWAGG_BIT_XOR:
+        case ROWAGG_SELECT_SOME:
         default:
         {
           AGG_MAP::iterator it = aggFuncMap.find(
@@ -2837,7 +2838,7 @@ void TupleAggregateStep::prep1PhaseDistinctAggregate(JobInfo& jobInfo, vector<Ro
                     f->fAggFunction == ROWAGG_STATS || f->fAggFunction == ROWAGG_BIT_AND ||
                     f->fAggFunction == ROWAGG_BIT_OR || f->fAggFunction == ROWAGG_BIT_XOR ||
                     f->fAggFunction == ROWAGG_CONSTANT || f->fAggFunction == ROWAGG_GROUP_CONCAT ||
-                    f->fAggFunction == ROWAGG_JSON_ARRAY))
+                    f->fAggFunction == ROWAGG_JSON_ARRAY || f->fAggFunction == ROWAGG_SELECT_SOME))
           {
             funct.reset(new RowAggFunctionCol(f->fAggFunction, f->fStatsFunction, f->fInputColumnIndex,
                                               f->fOutputColumnIndex, f->fAuxColumnIndex - multiParms));
@@ -3132,7 +3133,7 @@ void TupleAggregateStep::prep2PhasesAggregate(JobInfo& jobInfo, vector<RowGroup>
       {
         case ROWAGG_MIN:
         case ROWAGG_MAX:
-	case ROWAGG_SELECT_SOME:
+        case ROWAGG_SELECT_SOME:
         {
           oidsAggPm.push_back(oidsProj[colProj]);
           keysAggPm.push_back(aggKey);
@@ -4050,7 +4051,7 @@ void TupleAggregateStep::prep2PhasesDistinctAggregate(JobInfo& jobInfo, vector<R
       {
         case ROWAGG_MIN:
         case ROWAGG_MAX:
-	case ROWAGG_SELECT_SOME:
+        case ROWAGG_SELECT_SOME:
         {
           oidsAggPm.push_back(oidsProj[colProj]);
           keysAggPm.push_back(aggKey);
@@ -5086,7 +5087,7 @@ void TupleAggregateStep::prep2PhasesDistinctAggregate(JobInfo& jobInfo, vector<R
                      f->fAggFunction == ROWAGG_MIN || f->fAggFunction == ROWAGG_MAX ||
                      f->fAggFunction == ROWAGG_STATS || f->fAggFunction == ROWAGG_BIT_AND ||
                      f->fAggFunction == ROWAGG_BIT_OR || f->fAggFunction == ROWAGG_BIT_XOR ||
-                     f->fAggFunction == ROWAGG_CONSTANT)
+                     f->fAggFunction == ROWAGG_CONSTANT || f->fAggFunction == ROWAGG_SELECT_SOME)
             {
               funct.reset(new RowAggFunctionCol(f->fAggFunction, f->fStatsFunction, f->fInputColumnIndex,
                                                 f->fOutputColumnIndex, f->fAuxColumnIndex - multiParms));
