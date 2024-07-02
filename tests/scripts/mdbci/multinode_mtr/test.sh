ab72c46ec14cf903ecc9694f6c347988b7d8bc2b
Timofey Turenko <timofey.turenko@mariadb.com>
2024-07-02 13:02:57 +0300

    determine VM provider for maxscale VM

--- tests/scripts/mdbci/multinode_mtr/test.sh
+++ tests/scripts/mdbci/multinode_mtr/test.sh
@@ -5,7 +5,8 @@ export DRONE_BUILD_NUMBER=$2
 export box=$3
 export link_suff=$4
 ~/mdbci/mdbci generate-product-repositories --product mdbe_ci --product-version $target
-./columnstore_vm.py --machine-name $target-${box} --box ${box} --maxscale-box rocky_8_aws --target $target --server-product MariaDBEnterprise --num 3 --install-test-package True
+export provider=`echo ${box##*_}`
+./columnstore_vm.py --machine-name $target-${box} --box ${box} --maxscale-box rocky_8_$provider --target $target --server-product MariaDBEnterprise --num 3 --install-test-package True
 ./columnstore.py --machine-name $target-${box} --num 3 --storage gluster --aws True
 ./run_mtr.py --machine-name $target-${box} --node-name cs000 --mtr-param mtr-columnstore-test --target $target --product MariaDBEnterprise --use-packages True --install-packages False --multinode True --log-path $HOME/multinode_logs/ --build-number $DRONE_BUILD_NUMBER  --log-server localhost --log-server-user `whoami` 
 
