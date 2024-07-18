8f5512359282e4419de4237f34c45b8c1a39663c
Serguey Zefirov <serguey.zefirov@mariadb.com>
2024-07-18 15:03:23 +0300

    Fix build

--- dbcon/mysql/ha_mcs_execplan.cpp
+++ dbcon/mysql/ha_mcs_execplan.cpp
@@ -3431,10 +3431,10 @@ ReturnedColumn* searchCachedTransformedExpressions(Item* item, gp_walk_info& gwi
   }
   for(uint32_t i=0;i<gwi.retExprMap.size();i++)
   {
-    if (item->eq(gwi.retExprMap[i].get<0>()) &&
-        gwi.retExprMap[i].get<1>() == gwi.underAggregate)
+    if (item->eq(std::get<0>(gwi.retExprMap[i])) &&
+        set::get<1>(gwi.retExprMap[i]) == gwi.underAggregate)
     {
-      return gwi.retExprMap[i].get<2>().get();
+      return std::get<2>(gwi.retExprMap[i]).get();
     }
   }
   return nullptr;
