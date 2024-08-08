bf14a504617e0945fa3898c693fb3022f91447aa
Serguey Zefirov <serguey.zefirov@mariadb.com>
2024-08-08 17:55:59 +0300

    Debug logs

--- dbcon/mysql/ha_mcs_execplan.cpp
+++ dbcon/mysql/ha_mcs_execplan.cpp
@@ -3348,7 +3348,7 @@ ReturnedColumn* wrapIntoAggregate(ReturnedColumn* rc, gp_walk_info& gwi, Item* b
   }
 
   cal_connection_info* ci = static_cast<cal_connection_info*>(get_fe_conn_info_ptr());
-  idblog("new agg wrap, clause type " << int(gwi.clauseType) << ", wrapping: " << rc ? rc->toString() : "NULL");
+  idblog("new agg wrap, clause type " << int(gwi.clauseType) << ", wrapping: " << (rc ? rc->toString() : "NULL"));
 
   AggregateColumn* ac = new AggregateColumn(gwi.sessionid);
   ac->timeZone(gwi.timeZone);
@@ -4231,7 +4231,7 @@ ReturnedColumn* buildFunctionColumnBody(Item_func* ifp, gp_walk_info& gwi, bool&
             {
 		    idblog("cloning " << i << "th projection elem");
               ReturnedColumn* rc = gwi.returnedCols[j]->clone();
-	      idblog("cloned as " << rc ? rc->toString() : "NULL");
+	      idblog("cloned as " << (rc ? rc->toString() : "NULL"));
               rc->orderPos(j);
               sptp.reset(new ParseTree(rc));
               funcParms.push_back(sptp);
@@ -7815,7 +7815,7 @@ int getSelectPlan(gp_walk_info& gwi, SELECT_LEX& select_lex, SCSEP& csep, bool i
         {
           rc = buildFunctionColumn(ifp, gwi, hasNonSupportItem);
         }
-	idblog("func in select " << rc ? rc->toString() : "NULL");
+	idblog("func in select " << (rc ? rc->toString() : "NULL"));
 
         SRCP srcp(rc);
 
