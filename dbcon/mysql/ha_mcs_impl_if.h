c57f353bcb0cd2caf86f91579e6429b3fce6b7c9
Leonid Fedorov <leonid.fedorov@mariadb.com>
2024-08-13 13:54:45 +0000

    MCOL-4696: get rid of boost::iequals

--- dbcon/mysql/ha_mcs_impl_if.h
+++ dbcon/mysql/ha_mcs_impl_if.h
@@ -331,7 +331,7 @@ struct cal_connection_info
     configVal = cf->getConfig("SystemConfig", "PrimaryUMModuleName");
     std::string module = execplan::ClientRotator::getModule();
 
-    if (boost::iequals(configVal, module))
+    if (datatypes::ASCIIStringCaseInsensetiveEquals(configVal, module))
       return false;
 
     return true;
