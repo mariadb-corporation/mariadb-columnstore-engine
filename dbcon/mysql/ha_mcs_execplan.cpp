a0d1d568331718c2f07d1fd63bd192eb4cfe2d1e
Serguey Zefirov <serguey.zefirov@mariadb.com>
2024-10-11 14:54:48 +0300

    Some fixes

--- dbcon/mysql/ha_mcs_execplan.cpp
+++ dbcon/mysql/ha_mcs_execplan.cpp
@@ -2825,10 +2825,6 @@ void buildSubselectFunc(Item_func* ifp, gp_walk_info* gwip)
     // no need to check NULL for now. error will be handled in gp_walk
     gwip->ptWorkStack.push(subquery->transform());
     // recover original sub. Save current sub for Not handling.
-    if (gwip->lastSub)
-    {
-      delete gwip->lastSub;
-    }
     gwip->lastSub = subquery;
     gwip->subQuery = orig;
   }
@@ -6433,10 +6429,6 @@ void gp_walk(const Item* item, void* arg)
         // MIGR::infinidb_vtable.isUnion = true; // only temp. bypass the 2nd phase.
         // recover original
         gwip->subQuery = orig;
-	if (gwip->lastSub)
-	{
-          delete gwip->lastSub;
-	}
         gwip->lastSub = existsSub;
       }
       else if (sub->substype() == Item_subselect::IN_SUBS)
