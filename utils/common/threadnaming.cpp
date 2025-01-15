9dedc57df6e9da9efc6c088e15ad1b37cb5849f7
Daniel Black <daniel@mariadb.org>
2025-01-16 09:02:49 +1100

    feat(): MCOL-5881 set/getThreadName use FreeBSD API (#3381)
    Taken from FreeBSD ports, this uses the FreeBSD
    APIs rather than the Linux specific prctl to change
    and retreive the thread names.
    
    Co-authored-by: Bernard Spil <brnrd@FreeBSD.org>

--- utils/common/threadnaming.cpp
+++ utils/common/threadnaming.cpp
@@ -22,13 +22,21 @@ namespace utils
 {
 void setThreadName(const char* threadName)
 {
+#ifdef __FreeBSD__
+  pthread_set_name_np(pthread_self(), threadName);
+#else
   prctl(PR_SET_NAME, threadName, 0, 0, 0);
+#endif
 }
 
 std::string getThreadName()
 {
   char buf[32];
+#ifdef __FreeBSD__
+  pthread_get_name_np(pthread_self(), buf, sizeof(buf));
+#else
   prctl(PR_GET_NAME, buf, 0, 0, 0);
+#endif
   return std::string(buf);
 }
 }  // namespace utils
