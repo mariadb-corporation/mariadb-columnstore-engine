098527eb24fc6c708b4baa5b0a78ae02a08760d9
Aleksei Antipovskii <aleksei.antipovskii@mariadb.com>
2025-01-17 19:38:21 +0100

    use only long string storage for the group_concat data to reduce memory usage

--- dbcon/joblist/groupconcat.cpp
+++ dbcon/joblist/groupconcat.cpp
@@ -332,6 +332,7 @@ void GroupConcatAgUM::initialize()
   {
     fRowGroup = fGroupConcat->fRowGroup;
     fRowGroup.setUseStringTable(true);
+    fRowGroup.setUseOnlyLongString(true);
     fRowRGData.reinit(fRowGroup, 1);
     fRowGroup.setData(&fRowRGData);
     fRowGroup.resetRowGroup(0);
@@ -992,6 +993,7 @@ void GroupConcatNoOrder::initialize(const rowgroup::SP_GroupConcat& gcc)
   GroupConcator::initialize(gcc);
 
   fRowGroup = gcc->fRowGroup;
+  fRowGroup.setUseOnlyLongString(true);
   fRowsPerRG = 128;
   fErrorCode = ERR_AGGREGATION_TOO_BIG;
   fRm = gcc->fRm;
@@ -1004,7 +1006,7 @@ void GroupConcatNoOrder::initialize(const rowgroup::SP_GroupConcat& gcc)
 
   uint64_t newSize = fRowsPerRG * fRowGroup.getRowSize();
 
-  if (!fRm->getMemory(newSize, fSessionMemLimit))
+  if (fRm && !fRm->getMemory(newSize, fSessionMemLimit))
   {
     cerr << IDBErrorInfo::instance()->errorMsg(fErrorCode) << " @" << __FILE__ << ":" << __LINE__;
     throw IDBExcept(fErrorCode);
@@ -1014,6 +1016,7 @@ void GroupConcatNoOrder::initialize(const rowgroup::SP_GroupConcat& gcc)
 
   fData.reinit(fRowGroup, fRowsPerRG);
   fRowGroup.setData(&fData);
+  fRowGroup.setUseOnlyLongString(true);
   fRowGroup.resetRowGroup(0);
   fRowGroup.initRow(&fRow);
   fRowGroup.getRow(0, &fRow);
