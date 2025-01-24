d95c52e7302a36fd35d3ca02d3350f65ab66a68f
Leonid Fedorov <leonid.fedorov@mariadb.com>
2025-01-24 18:04:49 +0000

    chore(build): bump boost version

--- cmake/boost.cmake
+++ cmake/boost.cmake
@@ -37,8 +37,8 @@ ENDFOREACH()
 
 ExternalProject_Add(external_boost
   PREFIX .boost
-  URL https://archives.boost.io/release/1.79.0/source/boost_1_79_0.tar.bz2
-  URL_HASH SHA256=475d589d51a7f8b3ba2ba4eda022b170e562ca3b760ee922c146b6c65856ef39
+  URL https://archives.boost.io/release/1.84.0/source/boost_1_84_0.tar.gz
+  URL_HASH SHA256=a5800f405508f5df8114558ca9855d2640a2de8f0445f051fa1c7c3383045724
   CONFIGURE_COMMAND ./bootstrap.sh
   UPDATE_COMMAND ""
   BUILD_COMMAND ./b2 -q ${_b2args}
