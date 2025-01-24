6af91f49eb31b4eac939572be3a38bf43add6868
Leonid Fedorov <leonid.fedorov@mariadb.com>
2025-01-24 17:14:53 +0000

    chore(build): compile boost with gcc only

--- cmake/boost.cmake
+++ cmake/boost.cmake
@@ -22,7 +22,7 @@ SET(Boost_LIBRARY_DIRS "${INSTALL_LOCATION}/lib")
 LINK_DIRECTORIES("${Boost_LIBRARY_DIRS}")
 
 set(_cxxargs "-fPIC -DBOOST_NO_AUTO_PTR -fvisibility=default")
-set(_b2args cxxflags=${_cxxargs};cflags=-fPIC;threading=multi;${_extra};toolset=${_toolset} --without-python;--prefix=${INSTALL_LOCATION})
+set(_b2args cxxflags=${_cxxargs};cflags=-fPIC;threading=multi;${_extra}; --without-python;--prefix=${INSTALL_LOCATION})
 
 SET(byproducts)
 FOREACH(name chrono filesystem program_options regex system thread)
