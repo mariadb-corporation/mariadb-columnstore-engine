243c0df15a6afb9eb456d4358f86b940d43dd5dc
Serguey Zefiov <serguey.zefirov@mariadb.com>
2025-02-14 13:43:02 +0000

    fix(MCOL-4942): No-table-SELECT now can return empty set
    The query like "SELECT 1 WHERE 1=0" was returning a row despite
    unsatisfiable condition in WHERE. Now it returns an empty set.

--- dbcon/joblist/tupleconstantstep.h
+++ dbcon/joblist/tupleconstantstep.h
@@ -132,6 +132,7 @@ class TupleConstantOnlyStep : public TupleConstantStep
   uint32_t nextBand(messageqcpp::ByteStream& bs);
 
  protected:
+  bool fEmptySet;
   using TupleConstantStep::fillInConstants;
   void fillInConstants();
 };
