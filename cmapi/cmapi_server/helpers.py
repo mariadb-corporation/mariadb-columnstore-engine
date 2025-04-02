c37a0698caa1e3e495e84686de5f493af25e87d6
mariadb-AlanMologorsky <alan.mologorsky@mariadb.com>
2025-04-02 21:44:54 +0300

    feat(cmapi): MCOL-5806 Review and rebase interface fixes.
    * feat(cmapi): add read_only param for API add node endpoint
    * style(cmapi): fixes for string length and quotes

--- cmapi/cmapi_server/helpers.py
+++ cmapi/cmapi_server/helpers.py
@@ -543,7 +543,7 @@ def get_desired_nodes(config=DEFAULT_MCS_CONF_PATH):
 
 def get_read_only_nodes(root) -> list[str]:
     """Get names of read-only nodes from config"""
-    return [node.text for node in root.findall("./ReadOnlyNodes/Node")]
+    return [node.text for node in root.findall('./ReadOnlyNodes/Node')]
 
 
 def in_maintenance_state(config=DEFAULT_MCS_CONF_PATH):
