87a04242ed698750a36698c4027525ddf2aee757
Alexander Presnyakov <alexander.presniakov@mariadb.com>
2025-04-02 19:51:14 +0000

    Zero-diff to trigger package rebuild

--- cmapi/cmapi_server/helpers.py
+++ cmapi/cmapi_server/helpers.py
@@ -542,7 +542,7 @@ def get_desired_nodes(config=DEFAULT_MCS_CONF_PATH):
 
 
 def get_read_only_nodes(root) -> list[str]:
-    """Get names of read-only nodes from config"""
+    """Get names of read only nodes from config"""
     return [node.text for node in root.findall('./ReadOnlyNodes/Node')]
 
 
