0743e6b932cf9495bf477b7fc78c8ae3bf443ffb
Alexander Presnyakov <alexander.presniakov@mariadb.com>
2025-04-14 13:16:23 +0000

    Do not throw an exception on missing dbroot

--- versioning/BRM/extentmap.cpp
+++ versioning/BRM/extentmap.cpp
@@ -4541,7 +4541,7 @@ void ExtentMap::getDbRootHWMInfo(int OID, uint16_t pmNumber, EmDbRootHWMInfo_v& 
            "There are no DBRoots for OID "
         << OID << " and PM " << pmNumber << endl;
     log(oss.str(), logging::LOG_TYPE_CRITICAL);
-    throw invalid_argument(oss.str());
+    // throw invalid_argument(oss.str());
   }
 
   grabEMEntryTable(READ);
