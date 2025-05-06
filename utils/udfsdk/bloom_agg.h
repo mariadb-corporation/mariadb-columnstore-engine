c6ff8570c4d83ec2fe24ff065ead07ac323a8fa5
AestheticAkhmad <akhmadzoripov@gmail.com>
2025-05-06 15:35:17 +0200

    Syntax error fix and cleaned up comments

--- utils/udfsdk/bloom_agg.h
+++ utils/udfsdk/bloom_agg.h
@@ -71,7 +71,7 @@ struct BloomAggData : public mcsv1sdk::UserData
 {
    BloomAggData() {}
 
-   virtual ~BloomAggData() = {}
+   virtual ~BloomAggData() {}
 
    virtual void serialize(messageqcpp::ByteStream& stream) const override;
    virtual void unserialize(messageqcpp::ByteStream& stream) override;
@@ -82,123 +82,21 @@ private:
    BloomAggData(UserData&);
 };
 
-// Override mcsv1_UDAF to build your User Defined Aggregate (UDAF) and/or
-// User Defined Analytic Function (UDAnF).
-// These will be singleton classes, so don't put any instance
-// specific data in here. All instance data is stored in mcsv1Context
-// passed to each user function and retrieved by the getUserData() method.
-//
-// Each API function returns a ReturnCode. If ERROR is returned at any time,
-// the query is aborted, getInterrupted() will begin to return true and the
-// message set in config->setErrorMessage() is returned to MariaDB.
-
-// A simple aggregate to return the sum of squares
 class bloom_agg : public mcsv1_UDAF
 {
  public:
-  // Defaults OK
+
   bloom_agg() : mcsv1_UDAF(){};
   ~bloom_agg() override = default;
 
-  /**
-   * init()
-   *
-   * Mandatory. Implement this to initialize flags and instance
-   * data. Called once per SQL statement. You can do any sanity
-   * checks here.
-   *
-   * colTypes (in) - A vector of ColDataType defining the
-   * parameters of the UDA(n)F call. These can be used to decide
-   * to override the default return type. If desired, the new
-   * return type can be set by context->setReturnType() and
-   * decimal scale and precision can be set by context->setScale
-   * and context->setPrecision respectively.
-   *
-   * Return mcsv1_UDAF::ERROR on any error, such as non-compatible
-   * colTypes or wrong number of arguments. Else return
-   * mcsv1_UDAF::SUCCESS.
-   */
   ReturnCode init(mcsv1Context* context, ColumnDatum* colTypes) override;
 
-  /**
-   * reset()
-   *
-   * Mandatory. Reset the UDA(n)F for a new group, partition or,
-   * in some cases, new Window Frame. Do not free any memory
-   * allocated by context->createUserData(). The SDK Framework
-   * owns that memory and will handle that. Use this opportunity
-   * to reset any variables in context->getUserData() needed for
-   * the next aggregation. May be called multiple times on
-   * different modules.
-   */
   ReturnCode reset(mcsv1Context* context) override;
 
-  /**
-   * nextValue()
-   *
-   * Mandatory. Handle a single row.
-   *
-   * colsIn - A vector of data structure describing the input
-   * data.
-   *
-   * This function is called once for every row in the filtered
-   * result set (before aggregation). It is very important that
-   * this function is efficient.
-   *
-   * If the UDAF is running in a distributed fashion, nextValue
-   * cannot depend on order, as it will only be called for each
-   * row found on the specific PM.
-   *
-   * valsIn (in) - a vector of the parameters from the row.
-   */
   ReturnCode nextValue(mcsv1Context* context, ColumnDatum* valsIn) override;
 
-  /**
-   * subEvaluate()
-   *
-   * Mandatory -- Called if the UDAF is running in a distributed
-   * fashion. Columnstore tries to run all aggregate functions
-   * distributed, depending on context.
-   *
-   * Perform an aggregation on rows partially aggregated by
-   * nextValue. Columnstore calls nextValue for each row on a
-   * given PM for a group (GROUP BY). subEvaluate is called on the
-   * UM to consolodate those values into a single instance of
-   * userData. Keep your aggregated totals in context's userData.
-   * The first time this is called for a group, reset() would have
-   * been called with this version of userData.
-   *
-   * Called for every partial data set in each group in GROUP BY.
-   *
-   * When subEvaluate has been called for all subAggregated data
-   * sets, Evaluate will be called.
-   *
-   * valIn (In) - This is a pointer to a memory block of the size
-   * set in setUserDataSize. It will contain the value of userData
-   * as seen in the last call to NextValue for a given PM.
-   *
-   */
   ReturnCode subEvaluate(mcsv1Context* context, const UserData* userDataIn) override;
 
-  /**
-   * evaluate()
-   *
-   * Mandatory. Get the aggregated value.
-   *
-   * Called for every new group if UDAF GROUP BY, UDAnF partition
-   * or, in some cases, new Window Frame.
-   *
-   * Set the aggregated value into valOut. The datatype is assumed
-   * to be the same as that set in the init() function;
-   *
-   * If the UDAF is running in a distributed fashion, evaluate is
-   * called after a series of subEvaluate calls.
-   *
-   * valOut (out) - Set the aggregated value here. The datatype is
-   * assumed to be the same as that set in the init() function;
-   *
-   * To return a NULL value, don't assign to valOut.
-   */
   ReturnCode evaluate(mcsv1Context* context, static_any::any& valOut) override;
 
  protected:
