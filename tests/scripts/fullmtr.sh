a0861f8a13f7cc2ac62c6449587f46daf11db2ec
Leonid Fedorov <leonid.fedorov@mariadb.com>
2025-07-13 14:05:40 +0000

    start regressions after mtr logs published

--- tests/scripts/fullmtr.sh
+++ tests/scripts/fullmtr.sh
@@ -4,48 +4,43 @@ SCRIPT_LOCATION=$(dirname "$0")
 MARIADB_SOURCE_PATH=$(realpath $SCRIPT_LOCATION/../../../../../)
 COLUMNSTORE_MTR_SOURCE=$(realpath $SCRIPT_LOCATION/../../mysql-test/columnstore)
 INSTALLED_MTR_PATH='/usr/share/mysql/mysql-test'
-COLUMSNTORE_MTR_INSTALLED=${INSTALLED_MTR_PATH}/plugin/columnstore/columnstore/
+COLUMSNTORE_MTR_INSTALLED=${INSTALLED_MTR_PATH}/plugin/columnstore/
 PATCHNAME=$(realpath $SCRIPT_LOCATION)/mtr_warn.patch
-CURRENT_DIR=`pwd`
+CURRENT_DIR=$(pwd)
 mysql -e "create database if not exists test;"
-SOCKET=`mysql -e "show variables like 'socket';" | grep socket | cut -f2`
+SOCKET=$(mysql -e "show variables like 'socket';" | grep socket | cut -f2)
 
 export ASAN_OPTIONS=abort_on_error=1:disable_coredump=0,print_stats=false,detect_odr_violation=0,check_initialization_order=1,detect_stack_use_after_return=1,atexit=false,log_path=/core/asan.hz
-
 
 # needed when run MTR tests locally, see mariadb-test-run.pl:417, mtr functions
 # are added to the database mtr only when --extern is not specified
 
-add_mtr_warn_functions()
-{
+add_mtr_warn_functions() {
     echo "Adding mtr warnings functions..."
     cd /tmp
-    mysql -e "drop database if exists mtr";
+    mysql -e "drop database if exists mtr"
     cp ${MARIADB_SOURCE_PATH}/mysql-test/include/mtr_warnings.sql mtr_warnings.sql
-    patch -p1 < ${PATCHNAME}
+    patch -p1 <${PATCHNAME}
     mysql -e "create database if not exists mtr;"
-    mysql mtr < mtr_warnings.sql
+    mysql mtr <mtr_warnings.sql
     rm mtr_warnings.sql
     cd -
     echo "MTR Warnings function added"
 }
 
-
 cd ${INSTALLED_MTR_PATH}
 
-if [[ ! -d  ${COLUMSNTORE_MTR_INSTALLED} ]]; then
+if [[ ! -d ${COLUMSNTORE_MTR_INSTALLED} ]]; then
     echo ' ・ Adding symlink for columnstore tests to ${COLUMSNTORE_MTR_INSTALLED} from ${COLUMNSTORE_MTR_SOURCE}   '
     ln -s ${COLUMNSTORE_MTR_SOURCE} ${COLUMSNTORE_MTR_INSTALLED}
 fi
 
-
-if [[ ! -d  '/data/qa/source/dbt3/' || ! -d '/data/qa/source/ssb/' ]]; then
+if [[ ! -d '/data/qa/source/dbt3/' || ! -d '/data/qa/source/ssb/' ]]; then
     echo ' ・ Downloading and extracting test data for full MTR to /data'
     bash -c "wget -qO- https://cspkg.s3.amazonaws.com/mtr-test-data.tar.lz4 | lz4 -dc - | tar xf - -C /"
 fi
 
-run_suite()
-{
+run_suite() {
     ls /core >$CURRENT_DIR/mtr.$1.cores-before
     ./mtr --force --extern=socket=${SOCKET} --max-test-fail=0 --testcase-timeout=60 --suite=columnstore/$1 $2 | tee $CURRENT_DIR/mtr.$1.log 2>&1
     # dump analyses.
@@ -57,15 +52,14 @@ run_suite()
     rm $CURRENT_DIR/mtr.$1.cores-before $CURRENT_DIR/mtr.$1.cores-after
 }
 
-
 add_mtr_warn_functions
 
-if (( $# == 2 )); then
+if (($# == 2)); then
     run_suite $1 $2
     exit 1
 fi
 
-if (( $# == 1 )); then
+if (($# == 1)); then
     run_suite $1
     exit 1
 fi
