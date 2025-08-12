68da0d2a570ffbb5db75a1cd5a57af274c46b23e
mariadb-KristinaPavlova <kristina.pavlova@mariadb.com>
2025-08-12 13:17:49 +0300

    add solution only for the example

--- dbcon/mysql/ha_mcs_execplan_walks.cpp
+++ dbcon/mysql/ha_mcs_execplan_walks.cpp
@@ -251,8 +251,11 @@ void gp_walk(const Item* item, void* arg)
       Item* ncitem = const_cast<Item*>(item);
       Item_func* ifp = static_cast<Item_func*>(ncitem);
 
-      std::string funcName = ifp->func_name();
-
+      if(ifp->arguments()[0] != nullptr && ifp->arguments()[0]->type()!= Item::FIELD_ITEM)
+      {
+        return;
+      }
+      
       if (!gwip->condPush)
       {
         if (!ifp->fixed())
