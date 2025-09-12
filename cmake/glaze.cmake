78f84c1b1aa83276b975da872efd592f64128e01
Leonid Fedorov <leonid.fedorov@mariadb.com>
2025-09-12 19:36:09 +0000

    bump glzae version

--- cmake/glaze.cmake
+++ cmake/glaze.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
   glaze
   GIT_REPOSITORY https://github.com/stephenberry/glaze.git
-  GIT_TAG v5.7.0
+  GIT_TAG v5.7.1
   GIT_SHALLOW TRUE
 )
 FetchContent_MakeAvailable(glaze)
