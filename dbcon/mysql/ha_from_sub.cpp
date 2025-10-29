3beaec429710cf30a64fe59aac106cc01e9b8121
boosted-ape <wongboonjhee52@gmail.com>
2025-10-29 19:59:24 +0800

    prohibited groupby, orderby and window functions

--- dbcon/mysql/ha_from_sub.cpp
+++ dbcon/mysql/ha_from_sub.cpp
@@ -220,6 +220,7 @@ SCSEP FromSubQuery::transform()
   gwi.thd = fGwip.thd;
   gwi.subQuery = this;
   gwi.viewName = fGwip.viewName;
+  gwi.isRecursiveWithTable = fGwip.isRecursiveWithTable;
   csep->derivedTbAlias(fAlias);  // always lower case
   csep->derivedTbView(fGwip.viewName.alias, lower_case_table_names);
 
@@ -256,7 +257,7 @@ SCSEP FromSubQuery::transform(bool b)
   gwi.thd = fGwip.thd;
   gwi.subQuery = this;
   gwi.viewName = fGwip.viewName;
-  gwi.isRecursiveWithTable = true;
+  gwi.isRecursiveWithTable = fGwip.isRecursiveWithTable;
   csep->derivedTbAlias(fAlias);  // always lower case
   csep->derivedTbView(fGwip.viewName.alias, lower_case_table_names);
 
