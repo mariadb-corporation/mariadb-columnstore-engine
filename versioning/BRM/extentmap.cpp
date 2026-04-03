ce5ab0d4273071996ede256d3a54128726dd8b8a
Serguey Zefirov <serguey.zefirov@mariadb.com>
2026-04-03 09:21:39 +0000

    fix(cpimport): MCOL-6326 - cpimport should invalidate partially rolled back extents
    This patch adds invalidation logic for partially rolled back extents
    during rollback after a failure. Previously only HWMs were set, now
    ranges are specificially invalidated too.
    
    Test is in tests/custom/cpimport-rollback.sh, not in MTR.

--- versioning/BRM/extentmap.cpp
+++ versioning/BRM/extentmap.cpp
@@ -226,6 +226,14 @@ bool EMEntry::operator<(const EMEntry& e) const
   return false;
 }
 
+void EMEntry::setHWMAndInvalidate(HWM_t newHWM)
+{
+  HWM = newHWM;
+
+  partition.cprange.isValid = CP_INVALID;
+}
+
+
 /*static*/
 boost::mutex ExtentMap::mutex;
 boost::mutex ExtentMap::emIndexMutex;
@@ -3827,7 +3835,7 @@ void ExtentMap::rollbackColumnExtents_DBroot(int oid, bool bDeleteAll, uint16_t 
             if (emEntry.HWM != (fboLo - 1))
             {
               makeUndoRecordRBTree(UndoRecordType::DEFAULT, emEntry);
-              emEntry.HWM = fboLo - 1;  // case 3A
+              emEntry.setHWMAndInvalidate(fboLo - 1);  // case 3A
               emEntry.status = EXTENTAVAILABLE;
             }
           }
@@ -3845,7 +3853,7 @@ void ExtentMap::rollbackColumnExtents_DBroot(int oid, bool bDeleteAll, uint16_t 
           if (emEntry.HWM != fboHi)
           {
             makeUndoRecordRBTree(UndoRecordType::DEFAULT, emEntry);
-            emEntry.HWM = fboHi;  // case 4B
+            emEntry.setHWMAndInvalidate(fboHi);  // case 4B
             emEntry.status = EXTENTAVAILABLE;
           }
         }
@@ -3854,7 +3862,7 @@ void ExtentMap::rollbackColumnExtents_DBroot(int oid, bool bDeleteAll, uint16_t 
           if (emEntry.HWM != hwm)
           {
             makeUndoRecordRBTree(UndoRecordType::DEFAULT, emEntry);
-            emEntry.HWM = hwm;  // case 4C
+            emEntry.setHWMAndInvalidate(hwm);  // case 4C
             emEntry.status = EXTENTAVAILABLE;
           }
         }
