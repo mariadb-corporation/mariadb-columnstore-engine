8adc75275ffdba70f53a4caff841684e160767fc
Leonid Fedorov <leonid.fedorov@mariadb.com>
2026-04-07 01:32:38 +0000

    Fix Infer reports #1245-#1248: Initialize output params in sendPrimitiveMessages
    #1245 pdictionaryscan.cpp:352 Uninitialized Value - dbroot read without init
    #1246 pdictionaryscan.cpp:362 Uninitialized Value - oid read without init
    #1247 pdictionaryscan.cpp:362 Uninitialized Value - partNum read without init
    #1248 pdictionaryscan.cpp:362 Uninitialized Value - segNum read without init
    
    All four are output params from dbrm.lookupLocal() but should be initialized
    for safety in case lookupLocal fails silently.

--- dbcon/joblist/pdictionaryscan.cpp
+++ dbcon/joblist/pdictionaryscan.cpp
@@ -326,10 +326,10 @@ void pDictionaryScan::sendPrimitiveMessages()
   HWM_t hwm;
   uint32_t fbo;
   DBRM dbrm;
-  uint16_t dbroot;
-  uint32_t partNum;
-  uint16_t segNum;
-  BRM::OID_t oid;
+  uint16_t dbroot = 0;
+  uint32_t partNum = 0;
+  uint16_t segNum = 0;
+  BRM::OID_t oid = 0;
   boost::shared_ptr<map<int, int> > dbRootConnectionMap;
   boost::shared_ptr<map<int, int> > dbRootPMMap;
   oam::OamCache* oamCache = oam::OamCache::makeOamCache();
