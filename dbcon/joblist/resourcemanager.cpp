0d4d3c80e0a224bf89108c84f3f12d4584f10be0
Aleksei Antipovskii <aleksei.antipovskii@mariadb.com>
2026-07-03 17:59:25 +0200

    feat(PrimProc): MCOL-6441 cherry pick getMemoryForce method from 23.10

--- dbcon/joblist/resourcemanager.cpp
+++ dbcon/joblist/resourcemanager.cpp
@@ -389,4 +389,12 @@ bool ResourceManager::getMemory(int64_t amount, bool patience)
   return ret1;
 }
 
+bool ResourceManager::getMemoryForce(int64_t amount, boost::shared_ptr<int64_t>& sessionLimit)
+{
+  bool ret1 = (atomicops::atomicSubRef(totalUmMemLimit, amount) >= 0);
+  bool ret2 = sessionLimit ? (atomicops::atomicSub(sessionLimit.get(), amount) >= 0) : ret1;
+
+  return (ret1 && ret2);
+}
+
 }  // namespace joblist
