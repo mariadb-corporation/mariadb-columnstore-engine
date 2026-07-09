86c68fa522e4429f963a74a827e7d4661844ac16
Vasily Kozhukhovskiy <vasily.kozhukhovskiy@mariadb.com>
2026-07-09 14:50:16 +0000

    fix(storage-manager): MCOL-6429 pass putObject shared_ptr param by const reference

--- storage-manager/src/LocalStorage.h
+++ storage-manager/src/LocalStorage.h
@@ -35,7 +35,7 @@ class LocalStorage : public CloudStorage
   int getObject(const std::string& sourceKey, std::shared_ptr<uint8_t[]>* data,
                 size_t* size = nullptr) override;
   int putObject(const std::string& sourceFile, const std::string& destKey) override;
-  int putObject(const std::shared_ptr<uint8_t[]> data, size_t len, const std::string& destKey) override;
+  int putObject(const std::shared_ptr<uint8_t[]>& data, size_t len, const std::string& destKey) override;
   int deleteObject(const std::string& key) override;
   int copyObject(const std::string& sourceKey, const std::string& destKey) override;
   int exists(const std::string& key, bool* out) override;
