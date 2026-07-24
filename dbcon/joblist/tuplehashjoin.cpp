8caeb4ae32dd49e554c137544ca104856dc2d8d0
Aleksei Antipovskii <aleksei.antipovskii@mariadb.com>
2026-07-24 03:10:59 +0200

    fix(PrimProc): MCOL-6491 check if joiner was constructed before aborting it

--- dbcon/joblist/tuplehashjoin.cpp
+++ dbcon/joblist/tuplehashjoin.cpp
@@ -2059,7 +2059,10 @@ void TupleHashJoinStep::abort()
 
   // To prevent potential endless loop in bucketsToTables()
   for (auto& joiner : joiners)
-    joiner->abort();
+  {
+    if (joiner)
+      joiner->abort();
+  }
 
   if (djs.size())
   {
