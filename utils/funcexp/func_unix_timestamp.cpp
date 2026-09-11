ed3d81b819194704ceba1e6921d429a5e38dc25c
Serguey Zefirov <serguey.zefirov@mariadb.com>
2026-09-11 11:23:59 +0100

    Refactor unix_timestamp to use correct time zone and correct formulae

--- utils/funcexp/func_unix_timestamp.cpp
+++ utils/funcexp/func_unix_timestamp.cpp
@@ -84,7 +84,7 @@ int64_t Func_unix_timestamp::getIntVal(rowgroup::Row& row, FunctionParm& parm, b
     case CalpontSystemCatalog::CHAR:
     case CalpontSystemCatalog::VARCHAR:
     case CalpontSystemCatalog::TEXT:
-      val = dataconvert::DataConvert::stringToTimestamp(parm[0]->data()->getStrVal(row, isNull), ct.getTimeZone());
+      val = dataconvert::DataConvert::stringToDatetime(parm[0]->data()->getStrVal(row, isNull));
 
       if (val == -1)
       {
@@ -151,11 +151,8 @@ int64_t Func_unix_timestamp::getIntVal(rowgroup::Row& row, FunctionParm& parm, b
   }
 
   // same algorithm as my_time.c:my_system_gmt_sec
-  uint32_t loop;
   time_t tmp_t = 0;
-  int shift = 0;
-  struct tm *l_time, tm_tmp;
-  int64_t diff, my_time_zone = parm[1]->data()->getIntVal(row, isNull);
+  int64_t my_time_zone = ct.getTimeZone();
 
   if ((year == helpers::TIMESTAMP_MAX_YEAR) && (month == 1) && (day > 4))
   {
@@ -163,55 +160,9 @@ int64_t Func_unix_timestamp::getIntVal(rowgroup::Row& row, FunctionParm& parm, b
     shift = 2;
   }
 
-  tmp_t = (time_t)(((helpers::calc_mysql_daynr(year, month, day) - 719528) * 86400L + (int64_t)hour * 3600L +
-                    (int64_t)(min * 60 + sec)) -
-                   (time_t)my_time_zone);
-
-  localtime_r(&tmp_t, &tm_tmp);
-  l_time = &tm_tmp;
-
-  for (loop = 0; loop < 2 && (hour != (uint32_t)l_time->tm_hour || min != (uint32_t)l_time->tm_min ||
-                              sec != (uint32_t)l_time->tm_sec);
-       loop++)
-  {
-    int days = day - l_time->tm_mday;
-
-    if (days < -1)
-      days = 1; /* Month has wrapped */
-    else if (days > 1)
-      days = -1;
-
-    diff = (3600L * (int64_t)(days * 24 + ((int)hour - (int)l_time->tm_hour)) +
-            (int64_t)(60 * ((int)min - (int)l_time->tm_min)) + (int64_t)((int)sec - (int)l_time->tm_sec));
-    tmp_t += (time_t)diff;
-    localtime_r(&tmp_t, &tm_tmp);
-    l_time = &tm_tmp;
-  }
-
-  if (loop == 2 && hour != (uint32_t)l_time->tm_hour)
-  {
-    int days = day - l_time->tm_mday;
-
-    if (days < -1)
-      days = 1; /* Month has wrapped */
-    else if (days > 1)
-      days = -1;
-
-    diff = (3600L * (int64_t)(days * 24 + ((int)hour - (int)l_time->tm_hour)) +
-            (int64_t)(60 * ((int)min - (int)l_time->tm_min)) + (int64_t)((int)sec - (int)l_time->tm_sec));
-
-    if (diff == 3600)
-      tmp_t += 3600 - min * 60 - sec; /* Move to next hour */
-    else if (diff == -3600)
-      tmp_t -= min * 60 + sec; /* Move to previous hour */
-  }
-
-  /* shift back, if we were dealing with boundary dates */
-  tmp_t += shift * 86400L;
-
-  /* make sure we have legit timestamps (i.e. we didn't over/underflow anywhere above) */
-  if ((tmp_t < helpers::TIMESTAMP_MIN_VALUE) || (tmp_t > helpers::TIMESTAMP_MAX_VALUE))
-    tmp_t = 0;
+  tmp_t = ((helpers::calc_mysql_daynr(year, month, day) - 719528L) * 86400L + int64_t(hour) * 3600L +
+                    int64_t(min * 60L + sec)) -
+                   my_time_zone;
 
   return (int64_t)tmp_t;
 }
